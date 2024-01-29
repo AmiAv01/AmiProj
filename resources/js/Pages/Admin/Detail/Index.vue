ab345bfaf6e2ee907e62d4d706fee2226f5bced4
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-01-29 17:42:52 +0300

    feat: add page with detail list

---
+++ resources/js/Pages/Admin/Detail/Index.vue
@@ -0,0 +1,15 @@
+<template>
+    <AdminLayout>
+        <DetailList :details = "details" :pagination = "pagination" ></DetailList>
+    </AdminLayout>
+</template>
+
+<script setup>
+
+defineProps({
+    details:Array,
+    pagination: Array,
+})
+import AdminLayout from '../Components/AdminLayout.vue'
+import DetailList from '../Detail/DetailList.vue'
+</script>
