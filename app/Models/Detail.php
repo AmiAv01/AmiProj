d731c65d692fa777f156abe23fb176a5dbd9f37f
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-01-29 17:40:41 +0300

    feat: add protected field with detail Model fields

--- app/Models/Detail.php
+++ app/Models/Detail.php
@@ -8,4 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Detail extends Model
 {
     use HasFactory;
+    public $table = "detail";
+
+    protected $fillable = [
+        'dt_id',
+        'dt_code',
+        'dt_extcode',
+        'dt_extname',
+        'dt_type',
+        'dt_comment',
+        'dt_foto',
+        'dt_invoice',
+        'dt_netto',
+        'dt_oem',
+        'dt_baza',
+        'dt_cena',
+        'dt_prod',
+        'dt_ost',
+        'dt_ostc',
+        'dt_typec',
+        'dt_bp',
+        'dt_cargo',
+        'dt_e',
+        'dt_hs',
+        'dt_datep',
+        'dt_name',
+        'fr_code',
+        'dt_tp_ptype',
+        'lt_dt_acode'
+    ];
+
 }
