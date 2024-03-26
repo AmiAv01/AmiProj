433ef1d2330f13dbb3636a0196f64bc8f301f36f
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-03-26 09:48:02 +0300

    feat: add layout for registered users

---
+++ resources/js/Pages/User/Layouts/UserLayout.vue
@@ -0,0 +1,10 @@
+<template>
+    <Header> </Header>
+    <Main></Main>
+    <Footer></Footer>
+</template>
+<script setup>
+import Header from "./Header.vue";
+import Main from "./Main.vue";
+import Footer from "./Footer.vue";
+</script>
