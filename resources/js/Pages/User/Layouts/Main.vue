a23da1fc0ba85eadd2f42219190c7c4836cfb99a
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-03-26 09:47:45 +0300

    feat: add main page component

---
+++ resources/js/Pages/User/Layouts/Main.vue
@@ -0,0 +1,113 @@
+<template>
+    <main>
+        <section>
+            <div class="w-full mt-[50px]">
+                <div class="w-[1300px] flex justify-between mx-auto my-0">
+                    <div
+                        class="flex justify-around border border-gray-700 w-[60%] h-[600px] rounded-[15px]"
+                    >
+                        <img
+                            src="../../../../../public/generators.jpg"
+                            class="h-[200px] w-[200px] self-center ml-4"
+                            alt="#"
+                        />
+                        <div class="flex flex-col w-[40%] justify-center">
+                            <p class="text-7xl mb-8">Стартеры</p>
+                            <button
+                                type="button"
+                                class="flex justify-center items-center text-white bg-green-700 mr-2 w-[200px] hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-xl px-4 py-4 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                            >
+                                Перейти
+                                <svg
+                                    class="w-5 h-5 ml-6 text-gray-800 dark:text-white"
+                                    aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 14 10"
+                                >
+                                    <path
+                                        stroke="currentColor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M1 5h12m0 0L9 1m4 4L9 9"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="w-[38%]">
+                        <div
+                            class="flex justify-around border border-gray-700 mb-[20px] h-[290px] rounded-[15px]"
+                        >
+                            <img
+                                src="../../../../../public/generators.jpg"
+                                class="h-[100px] w-[100px] self-center ml-4"
+                                alt="#"
+                            />
+                            <div class="flex flex-col justify-center w-[40%]">
+                                <p class="text-4xl mb-4">Генераторы</p>
+                                <button
+                                    type="button"
+                                    class="flex justify-center items-center text-white bg-green-700 mr-2 w-[160px] hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-md px-4 py-3 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                                >
+                                    Перейти
+                                    <svg
+                                        class="w-5 h-5 ml-6 text-gray-800 dark:text-white"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 10"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div
+                            class="flex justify-around border border-gray-700 h-[290px] rounded-[15px]"
+                        >
+                            <img
+                                src="../../../../../public/generators.jpg"
+                                class="h-[100px] w-[100px] self-center ml-4"
+                                alt="#"
+                            />
+                            <div class="flex flex-col justify-center w-[40%]">
+                                <p class="text-4xl mb-4">Остальные запчасти</p>
+                                <button
+                                    type="button"
+                                    class="flex justify-center items-center text-white bg-green-700 mr-2 w-[160px] hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-md px-4 py-3 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+                                >
+                                    Перейти
+                                    <svg
+                                        class="w-5 h-5 ml-6 text-gray-800 dark:text-white"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 10"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            stroke-width="2"
+                                            d="M1 5h12m0 0L9 1m4 4L9 9"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script setup></script>
