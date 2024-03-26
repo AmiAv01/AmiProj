84a3dd91cb18cd4b6895c9e4cbfbd34b3452236a
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-03-26 09:51:38 +0300

    feat: add user controller

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+
+class UserController extends Controller
+{
+    public function index(){
+        return Inertia::render('User/Index', [
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+            'laravelVersion' => Application::VERSION,
+            'phpVersion' => PHP_VERSION,
+        ]);
+    }
+}
