df5b8fd6306fa9258db1cf08634a42bf337f9c32
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-03-29 19:16:37 +0300

    feat: add details data

--- resources/js/Pages/User/Layouts/UserLayout.vue
+++ resources/js/Pages/User/Layouts/UserLayout.vue
@@ -1,10 +1,13 @@
 <template>
     <Header> </Header>
-    <Main></Main>
+    <Main v-bind:details="details"></Main>
     <Footer></Footer>
 </template>
 <script setup>
 import Header from "./Header.vue";
 import Main from "./Main.vue";
 import Footer from "./Footer.vue";
+defineProps({
+    details: Array,
+});
 </script>
