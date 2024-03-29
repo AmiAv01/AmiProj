277c6ae7493e2808f262ca257416469d8abe0560
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-03-29 19:11:40 +0300

    feat: get details paginate data and push for vue page

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Inertia\Inertia;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $details = DB::table('detail')->paginate(12);
+
         return Inertia::render('User/Index', [
+            'details' => $details,
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
             'laravelVersion' => Application::VERSION,
