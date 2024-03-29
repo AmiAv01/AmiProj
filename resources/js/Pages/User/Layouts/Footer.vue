3b9d95ee3ecbf6bee11d6703d96f74ce1743d6ab
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-03-29 19:13:32 +0300

    feat: add footer

---
+++ resources/js/Pages/User/Layouts/Footer.vue
@@ -0,0 +1,42 @@
+<template>
+    <footer class="bg-slate-900">
+        <div class="w-[1300px] mx-auto flex justify-around pb-32 pt-16">
+            <div class="">
+                <p class="text-white text-2xl mb-2">Ами-Авто</p>
+                <p class="mb-2 text-white">
+                    <i class="fa-solid fa-location-dot text-white mr-4"> </i
+                    >Минская область , Минский район , а/г. Ждановичи , ул. Мира
+                    12
+                </p>
+                <p class="text-white mb-2">
+                    <i class="fa-solid fa-envelope text-white mr-2"></i>+375 29
+                    630-85-35
+                </p>
+                <p class="text-white mb-2">
+                    <i class="fa-solid fa-phone text-white mr-2"></i
+                    >AMIAUTO.MINSK@GMAIL.COM
+                </p>
+                <p class="text-white">
+                    <i class="fa-brands fa-skype text-white mr-2"></i>AMI.AUTO
+                </p>
+            </div>
+            <div class="flex flex-col">
+                <p class="text-2xl text-white mb-2">О компании</p>
+                <p class="text-white mb-2">Новости</p>
+                <p class="text-white mb-2">О нас</p>
+                <p class="text-white mb-2">Контакты</p>
+                <p class="text-white mb-2">Как найти</p>
+            </div>
+            <div class="flex flex-col">
+                <p class="text-white text-2xl mb-2">Категории</p>
+                <p class="text-white mb-2">Генераторы</p>
+                <p class="text-white mb-2">Стартеры</p>
+                <p class="text-white">Остальные запчасти</p>
+            </div>
+        </div>
+        <p class="text-white text-center pb-2">
+            <i class="fa-regular fa-copyright"></i> Ами-Авто 2024-...
+        </p>
+    </footer>
+</template>
+<script setup></script>
