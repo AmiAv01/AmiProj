6ae595d84da8ccc6e15f287bef7a0097bfb9efa1
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-03-31 12:04:53 +0300

    feat: add auth props

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -1,10 +1,21 @@
 <template>
-    <UserLayout v-bind:details="details"> </UserLayout>
+    <UserLayout
+        v-bind:details="details"
+        v-bind:canLogin="canLogin"
+        v-bind:canRegister="canRegister"
+    >
+    </UserLayout>
 </template>
 
 <script setup>
 import UserLayout from "@/Pages/User/Layouts/UserLayout.vue";
 defineProps({
-    details: Array,
+    details: Object,
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
 });
 </script>
