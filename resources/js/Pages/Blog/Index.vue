6fcb2cfa8a05511e4a906e763fb1950c0bfa28b3
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-04-25 13:44:43 +0300

    feat: add blog component to show company news

---
+++ resources/js/Pages/Blog/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <layout>
+        <news />
+    </layout>
+</template>
+
+<script>
+import UserLayout from "@/Shared/UserLayout.vue";
+import News from "@/Shared/News.vue";
+
+export default {
+    components: {
+        layout: UserLayout,
+        news: News,
+    },
+};
+</script>
