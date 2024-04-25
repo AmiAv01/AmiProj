d22fc3387eada9847d3cd98dcc2626960e4381a9
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-04-25 13:43:05 +0300

    fix: replace Pagination component

---
+++ resources/js/Shared/Pagination.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex justify-center" v-if="links.length > 3">
+        <div class="flex flex-wrap mt-8">
+            <template v-for="(link, key) in links" :key="key">
+                <div
+                    v-if="link.url === null"
+                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 text-gray-400 border rounded"
+                    v-html="link.label"
+                />
+
+                <Link
+                    v-else
+                    class="mr-1 mb-1 px-4 py-3 text-sm leading-4 border rounded hover:bg-blue-200 focus:border-primary focus:text-primary"
+                    :class="{ 'bg-blue-200': link.active }"
+                    :href="link.url"
+                    preserve-scroll
+                    v-html="link.label"
+                />
+            </template>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { Link } from "@inertiajs/inertia-vue3";
+export default defineComponent({
+    components: {
+        Link,
+    },
+    props: {
+        links: Array,
+    },
+});
+</script>
