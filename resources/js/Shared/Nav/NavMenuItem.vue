800816257d3e570b08917940f91ce09c5de2b781
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-06-20 14:06:01 +0300

    feat: add nav menu item to nav menu

---
+++ resources/js/Shared/Nav/NavMenuItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+defineProps({
+    value: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
+});
+</script>
+
+<template>
+    <li class="px-4 py-4 border-r-2 border-white">
+        <Link
+            :href="`${link}`"
+            class="text-white w-full block group/link relative"
+            ><span
+                class="absolute top-10 left-0 w-0 h-0.5 bg-white group-hover/link:w-full group-hover:transition-all"
+            ></span
+            >{{ value }}</Link
+        >
+    </li>
+</template>
