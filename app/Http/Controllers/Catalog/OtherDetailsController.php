e646d97554f04a67e75330ca5e44f7cfd8ec0e40
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-06-20 13:56:02 +0300

    feat: add controller to control other details info

---
+++ app/Http/Controllers/Catalog/OtherDetailsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Catalog;
+
+use App\Http\Controllers\Controller;
+use App\Services\Detail\DetailService;
+use Inertia\Inertia;
+
+class OtherDetailsController extends Controller
+{
+    public function __construct(protected DetailService $detailService)
+    {
+
+    }
+
+    public function index()
+    {
+        $details = $this->detailService->getByCategory(['АВТОХИМИЯ', 'ВИНТ', 'ВЕНТИЛЯТОР САЛОНА', 'ВТУЛКА СФЕРИЧЕСКАЯ', 'ИЗОЛЯТОР', 'ИНСТРУМЕНТ',
+            'КРЫШКА ПОДШИПНИКА', 'МУФТА РЕЗИНОВАЯ', 'ОБОРУДОВАНИЕ И ОСНАСТКА', 'ПРИПОЙ ДЛЯ КОНТАКТНОЙ СВАРКИ', 'РЕМКОМПЛЕКТ', 'СМАЗКА ДЛЯ БЕНДИКСОВ MOLYKOTE 33',
+            'СТОПОР', 'СТОПОРНАЯ ПЛАСТИНА ПОДШИПНИКА', 'СТОПОРНОЕ КОЛЬЦО', 'СТОПОРНОЕ КОЛЬЦО КОМПЛЕКТ', 'ШАЙБА', 'ШАЙБА ДИСТАНЦИОННАЯ', 'ШПОНКА']);
+
+        return Inertia::render('Catalog/Index', [
+            'details' => $details,
+            'title' => 'Прочие запчасти',
+        ]);
+    }
+}
