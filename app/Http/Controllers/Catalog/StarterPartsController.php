68cbb22073199ec351cebb67d477cb4a9b426525
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-06-20 13:56:58 +0300

    feat: add controller to control starter parts details info

---
+++ app/Http/Controllers/Catalog/StarterPartsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Catalog;
+
+use App\Http\Controllers\Controller;
+use App\Services\Detail\DetailService;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+
+class StarterPartsController extends Controller
+{
+    private array $categories = ['fork' => 'ВИЛКА СТАРТЕРА', 'sleeve' => ['ВТУЛКА СТАРТЕРА', 'ВТУЛКА СТАРТЕРА КОМПЛЕКТ', 'НАБОР ВТУЛОК СТАРТЕРА'],
+        'relay' => 'ВТЯГИВАЮЩЕЕ РЕЛЕ СТАРТЕРА', 'relay_parts' => 'СЕРДЕЧНИК', 'unit_parts' => 'ШЕСТЕРНЯ ПРИВОДА СТАРТЕРА',
+        'cover' => 'КРЫШКА СТАРТЕРА', 'unit' => ['ПРИВОД СТАРТЕРА', 'РОЛИК ПРИВОДА СТАРТЕРА', 'ШТОК ПРИВОДА СТАРТЕРА'],
+        'other' => ['ДЕРЖАТЕЛЬ ВИЛКИ', 'РЕДУКТОР+ПРИВОД', 'КОНТАКТ(ШИНА) СТАРТЕРА', 'ЗАЩИТНЫЙ КОЛПАЧЁК СТАРТЕРА', 'ВИНТ СТАРТЕРА', 'ПРОВОД СТАРТЕРА', 'МУФТА СТАРТЕРА'],
+        'reducer' => ['ВАЛ РЕДУКТОРА СТАРТЕРА', 'ПЛАНЕТАРНЫЙ РЕДУКТОР', 'РЕДУКТОР СТАРТЕРА', 'КРЫШКА РЕДУКТОРА', 'КОРПУС РЕДУКТОРА'], 'repair' => 'РЕМКОМПЛЕКТ СТАРТЕРА',
+        'glands' => 'САЛЬНИК СТАРТЕРА', 'stator' => ['СТАТОР', 'КОРПУС СТАТОРА', 'СТАТОР В СБОРЕ С КРЫШКОЙ', 'СТАТОРНАЯ ОБМОТКА СТАРТЕРА'], 'brush' => 'ЩЁТКИ СТАРТЕРА',
+        'holder' => 'ЩЁТОЧНЫЙ УЗЕЛ СТАРТЕРА', 'anchor' => ['ЯКОРЬ СТАРТЕРА', 'КРЫШКА ЯКОРЯ']];
+
+    private array $names = ['fork' => 'Вилки стартера', 'sleeve' => 'Втулки стартера', 'relay' => 'Втягивающие реле', 'relay_parts' => 'Детали втягивающего реле',
+        'unit_parts' => 'Детали привода стартера', 'cover' => 'Крышки и головы', 'unit' => 'Приводы', 'other' => 'Прочее для стартера', 'reducer' => 'Редукторы и компоненты',
+        'repair' => 'Ремкомплекты', 'glands' => 'Сальники стартера', 'stator' => 'Статоры', 'brush' => 'Щётки стартера', 'holder' => 'Щёткодержатели стартера',
+        'anchor' => 'Якори стартера'];
+
+    public function __construct(protected DetailService $detailService)
+    {
+    }
+
+    public function index(string $category)
+    {
+        Log::channel('stderr')->info($this->categories[$category]);
+        $details = $this->detailService->getByCategory(is_array($this->categories[$category]) ? $this->categories[$category] : [$this->categories[$category]]);
+        Log::channel('stderr')->info($details);
+
+        return Inertia::render('Catalog/Index', [
+            'details' => $details,
+            'title' => $this->names[$category],
+        ]);
+    }
+}
