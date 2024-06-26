2a8ecd7b33e03e26aad7d45c35c1e3b8f61e2a06
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-06-26 15:26:28 +0300

    fix: change DB package to model pagination

--- app/Repositories/DetailRepository.php
+++ app/Repositories/DetailRepository.php
@@ -4,13 +4,12 @@ namespace App\Repositories;
 
 use App\Models\Detail;
 use App\Repositories\Interfaces\DetailRepositoryInterface;
-use Illuminate\Support\Facades\DB;
 
 class DetailRepository implements DetailRepositoryInterface
 {
     public function all()
     {
-        return DB::table('detail')->paginate(12);
+        return Detail::paginate(12);
     }
 
     public function findById(int $id)
@@ -18,8 +17,8 @@ class DetailRepository implements DetailRepositoryInterface
         return Detail::where('dt_id', '=', $id)->get();
     }
 
-    public function findByCategory(array $categories)
+    public function findByCategory(array $categories, array $brands)
     {
-        return DB::table('detail')->whereIn('dt_typec', $categories)->paginate(12);
+        return Detail::whereIn('dt_typec', $categories)->whereIn('fr_code', $brands)->paginate(12)->withQueryString();
     }
 }
