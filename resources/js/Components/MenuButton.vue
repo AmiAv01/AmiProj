65e80541aa4bf282a283913c34f81e7c37495bb8
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-05 13:35:02 +0300

    fix: replaced menu button to another component

---
+++ resources/js/Components/MenuButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <inertia-link
+        :href="`${href}`"
+        :class="`flex items-center text-white bg-green-700  hover:bg-green-800  font-medium rounded-lg  ${attributes} text-center dark:bg-green-600  `"
+    >
+        <slot />
+    </inertia-link>
+</template>
+
+<script>
+export default {
+    props: {
+        href: "",
+        attributes: {
+            type: String,
+            default: "",
+        },
+    },
+};
+</script>
