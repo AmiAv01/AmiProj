c9096ea6dce1bff279df07a26e60c26542fe93b3
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-05 13:40:12 +0300

    fix: replaced add-to-cart button to another component

--- resources/js/Pages/Card/Index.vue
+++ resources/js/Pages/Card/Index.vue
@@ -1,8 +1,8 @@
 <template>
     <layout>
         <section class="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
-            <div class="max-w-screen-xl px-4 mx-auto 2xl:px-0">
-                <div class="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+            <div class="w-full px-4 2xl:px-0">
+                <div class="lg:grid px-4 lg:grid-cols-3 lg:gap-8 xl:gap-16">
                     <div class="shrink-0 max-w-md lg:max-w-lg mx-auto">
                         <img
                             class="w-full dark:hidden"
@@ -43,10 +43,17 @@
                         <div
                             class="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8"
                         >
+                            <cart-button
+                                @click="addInCart"
+                                title=""
+                                class="bg-green-700 hover:bg-green-500 text-lg text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                                role="button"
+                            >
+                            </cart-button>
                             <a
                                 href="#"
                                 title=""
-                                class="bg-green-700 hover:bg-green-500 text-lg text-white mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                                class="flex items-center justify-center py-2.5 px-5 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                 role="button"
                             >
                                 <svg
@@ -63,43 +70,24 @@
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6"
+                                        d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
                                     />
                                 </svg>
-
-                                <span class="pl-4">Добавить в корзину</span>
+                                <span class="pl-4">Добавить в избранное</span>
                             </a>
-                            <input-number />
                         </div>
 
                         <hr
                             class="my-6 md:my-8 border-gray-200 dark:border-gray-800"
                         />
-                        <a
-                            href="#"
-                            title=""
-                            class="flex items-center justify-center py-2.5 px-5 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                            role="button"
-                        >
-                            <svg
-                                class="w-9 h-9 -ms-2 me-2"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-                                />
-                            </svg>
-                            <span class="pl-4">Добавить в избранное</span>
-                        </a>
+                    </div>
+                    <div class="rounded-lg border-2">
+                        <p class="text-2xl px-4 py-4 text-center border-b-2">
+                            <strong>Деталировка</strong>
+                        </p>
+                        <div class="h-[400px] overflow-y-auto">
+                            <detail-list :details="sameDetails" />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -108,16 +96,38 @@
 </template>
 
 <script>
-import UserLayout from "@/Shared/UserLayout.vue";
-import InputNumber from "@/Components/InputNumber.vue";
+import axios from "axios";
 
 export default {
-    components: {
-        layout: UserLayout,
-        "input-number": InputNumber,
+    created() {
+        console.log(this.sameDetails);
     },
-    props: {
-        detail: Array,
+    methods: {
+        addInCart() {
+            axios
+                .post("/cart", {
+                    id: this.detail[0].dt_id,
+                    typec: this.detail[0].dt_typec,
+                    invoice: this.detail[0].dt_invoice,
+                    cargo: this.detail[0].dt_cargo,
+                    fr_code: this.detail[0]._code,
+                })
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err));
+        },
     },
 };
+</script>
+
+<script setup>
+import axios from "axios";
+
+defineProps({
+    sameDetails: {
+        type: Array,
+    },
+    detail: {
+        type: Array,
+    },
+});
 </script>
