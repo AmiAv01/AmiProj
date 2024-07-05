3d411bf088349e26026df890b4ccc4089b7b1f6b
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-05 13:38:01 +0300

    fix: replaced detail list to another component

---
+++ resources/js/Shared/DetailList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div
+            v-for="detail in details"
+            :key="detail.dt_id"
+            class="bg-white w-full p-4 flex border-b-2"
+        >
+            <div class="mr-4">
+                <img
+                    v-if="detail.dt_foto.length === 0"
+                    :alt="detail"
+                    class="w-10 h-10 object-cover object-center"
+                />
+                <img
+                    v-else=""
+                    src="../../../public/build/no-photo--lg.png"
+                    alt="#"
+                    class="w-10 h-10 object-cover object-center"
+                />
+            </div>
+            <div class="flex flex-col">
+                <p class="font-manrope font-semibold text-lg text-black mb-2">
+                    <a use:inertia-vue3 :href="`../product/${detail.dt_id}`">
+                        <span aria-hidden="true" />
+                        {{ detail.dt_typec }}
+                        {{ detail.dt_invoice }}
+                    </a>
+                </p>
+                <p class="font-normal text-md text-gray-500">
+                    Артикул: {{ detail.dt_cargo }}
+                </p>
+                <p class="font-normal text-md text-gray-500">
+                    Бренд: {{ detail.fr_code }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        details: Array,
+    },
+};
+</script>
