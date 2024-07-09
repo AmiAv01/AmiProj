df7b8fe57af3975f692acb03d2179b1228f6c09f
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-09 13:49:47 +0300

    fix: remove add to favorite button

--- resources/js/Pages/Card/Index.vue
+++ resources/js/Pages/Card/Index.vue
@@ -50,31 +50,6 @@
                                 role="button"
                             >
                             </cart-button>
-                            <a
-                                href="#"
-                                title=""
-                                class="flex items-center justify-center py-2.5 px-5 text-lg font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                                role="button"
-                            >
-                                <svg
-                                    class="w-9 h-9 -ms-2 me-2"
-                                    aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"
-                                    />
-                                </svg>
-                                <span class="pl-4">Добавить в избранное</span>
-                            </a>
                         </div>
 
                         <hr
