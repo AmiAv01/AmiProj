e7ade1846eae2a804bf8a059d2074714405c697b
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-09 13:54:48 +0300

    feat: add order link for auth users

--- resources/js/Shared/Header.vue
+++ resources/js/Shared/Header.vue
@@ -111,7 +111,7 @@
                         </ul>
                     </div>
                     <menu-button
-                        :attributes="`px-4 py-2 text-sm`"
+                        :attributes="`px-4 py-2 text-sm mr-2`"
                         :href="route('cart.index')"
                     >
                         <svg
@@ -124,6 +124,28 @@
                             ></path>
                         </svg>
                         Корзина
+                    </menu-button>
+                    <menu-button
+                        :attributes="`px-4 py-2 text-sm ml-2`"
+                        :href="route('cart.index')"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            class="w-4 h-4 mr-2 fill-white"
+                            width="24"
+                            height="24"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                            stroke="#000000"
+                            stroke-width="2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        >
+                            <path
+                                d="M6 2L3 6v14c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2V6l-3-4H6zM3.8 6h16.4M16 10a4 4 0 1 1-8 0"
+                            />
+                        </svg>
+                        Заказы
                     </menu-button>
                 </div>
                 <div
