0636d898ad6e10adf77f5a2d0335144258413ca0
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-18 13:54:23 +0300

    feat: add order items model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class OrderItem extends Model
 {
     use HasFactory;
+
+    public $table = 'order_item';
+
+    protected $fillable = [
+        'detail_id',
+        'quantity',
+        'unit_price',
+    ];
+
+    public function getDetail()
+    {
+        return $this->belongsTo(Detail::class);
+    }
 }
