2f310f7a77c3c525004b12d96e4a9be23a147005
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-18 13:55:52 +0300

    feat: add news service

---
+++ app/Services/NewsService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\News;
+
+class NewsService
+{
+    public function getAll()
+    {
+        return News::all();
+    }
+
+    public function destroy()
+    {
+
+    }
+
+    public function show()
+    {
+    }
+
+    public function update()
+    {
+    }
+}
