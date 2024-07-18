4397f06286f750ddf6f46d8f4514c088b53f9967
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-18 14:15:15 +0300

    feat: add order list for clients

---
+++ resources/js/Pages/Order/OrderList.vue
@@ -0,0 +1,65 @@
+<template>
+    <layout>
+        <p class="text-5xl font-bold tracking-tight text-gray-900 ml-10 py-24">
+            Заказы
+        </p>
+        <div class="relative overflow-x-auto w-[80%] mx-auto pb-10 rounded-lg">
+            <table
+                class="w-full text-lg text-left rtl:text-right text-gray-500 dark:text-gray-400"
+            >
+                <thead
+                    class="text-xl text-white uppercase bg-green-700 rounded-lg"
+                >
+                    <tr class="rounded-[15px]">
+                        <th scope="col" class="px-8 py-6">№ заказа</th>
+                        <th scope="col" class="px-8 py-6">Дата</th>
+                        <th scope="col" class="px-8 py-6">Сумма</th>
+                        <th scope="col" class="px-8 py-6">Статус</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="(order, index) in orders"
+                        :key="index"
+                        @click="setLink(order.id)"
+                        class="bg-white w-full border-b text-lg dark:bg-gray-800 cursor-pointer dark:border-gray-700"
+                    >
+                        <th
+                            scope="row"
+                            class="px-8 py-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                            {{ order.id }}
+                        </th>
+                        <td class="px-8 py-6">
+                            {{ order.created_at }}
+                        </td>
+                        <td class="px-8 py-6">
+                            {{ order.total_price }}
+                        </td>
+                        <td class="px-8 py-6">{{ order.status }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </layout>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log(this.orders);
+    },
+    methods: {
+        setLink(id) {
+            console.log(id);
+            window.location = `order/${id}`;
+        },
+    },
+};
+</script>
+
+<script setup>
+defineProps({
+    orders: Array,
+});
+</script>
