994c47ea86f2ff097126e098e11bab697fdd3ac0
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-18 14:07:48 +0300

    feat: add global components

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,8 @@ import DetailList from "@/Shared/DetailList.vue";
 import UserLayout from "@/Shared/UserLayout.vue";
 import MenuButton from "@/Components/MenuButton.vue"
 import CartButton from "@/Components/CartButton.vue";
+import Pagination from "@/Shared/Pagination.vue"
+import Search from "@/Shared/Search.vue"
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -18,7 +20,8 @@ createInertiaApp({
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
         const app = createApp({ render: () => h(App, props) });
-        app.component('inertia-link', Link).component('detail-list', DetailList).component('layout', UserLayout).component('menu-button', MenuButton).component('cart-button', CartButton);
+        app.component('inertia-link', Link).component('detail-list', DetailList).component('layout', UserLayout).
+            component('menu-button', MenuButton).component('cart-button', CartButton).component('pagination', Pagination);
         return app.use(plugin)
         .use(ZiggyVue, Ziggy)
         .mount(el);
