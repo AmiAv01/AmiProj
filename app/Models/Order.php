e469241b5270c60b96819a9bc7ffa5a2896cd71a
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-18 13:54:04 +0300

    feat: add order model with orderItems link

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    public $table = 'order';
+
+    protected $fillable = [
+        'total_price',
+        'status',
+        'session_id',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
