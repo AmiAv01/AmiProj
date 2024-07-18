e19fc2e8cf979008706952193395ef5a6f6c5187
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-18 14:08:28 +0300

    feat: add button for admin panel

---
+++ resources/js/Components/AdminButton.vue
@@ -0,0 +1,18 @@
+<template>
+    <inertia-link
+        :href="route(`${href}`)"
+        class="flex items-center p-6 pl-11 w-full text-lg font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+        ><slot
+    /></inertia-link>
+</template>
+
+<script>
+export default {
+    props: {
+        href: {
+            type: String,
+            default: "#",
+        },
+    },
+};
+</script>
