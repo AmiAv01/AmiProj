165b08c313864cc21655c42e6037ac0ff9c9b107
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-18 13:51:28 +0300

    feat: add news service for the news controller

--- app/Http/Controllers/News/NewsController.php
+++ app/Http/Controllers/News/NewsController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers\News;
 
 use App\Http\Controllers\Controller;
+use App\Services\NewsService;
 use Inertia\Inertia;
 
 class NewsController extends Controller
 {
+    public function __construct(protected NewsService $newsService)
+    {
+
+    }
+
     public function index()
     {
-        return Inertia::render('Blog/Index');
+        return Inertia::render('Blog/Index', ['news' => $this->newsService->getAll()]);
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
     }
 }
