b3078fa84f43d8f2367f829f1d01c97f03c69530
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-25 17:08:54 +0300

    feat: rename menu-button

--- resources/js/Pages/Admin/Components/Sidebar.vue
+++ resources/js/Pages/Admin/Components/Sidebar.vue
@@ -38,7 +38,7 @@
                     <admin-button
                         :href="`admin.details.index`"
                         class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                        >Продукты</admin-button
+                        >Запчасти</admin-button
                     >
                 </li>
                 <li>
