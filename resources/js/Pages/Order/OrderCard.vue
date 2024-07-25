786e929e1c41bc691ac1313519bbbe87004917b1
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-25 17:16:10 +0300

    fix: change tailwind styling

--- resources/js/Pages/Order/OrderCard.vue
+++ resources/js/Pages/Order/OrderCard.vue
@@ -1,29 +1,27 @@
 <template>
     <layout>
-        <div class="flex p-10 w-[80%] mx-auto justify-around">
+        <div class="flex flex-col p-10 w-[60%] mx-auto justify-around">
             <div
-                class="flex pl-10 bg-gray-200 rounded-lg pb-10 w-[500px] max-h-[300px] flex-col"
+                class="flex rounded-[15px] pb-10 w-[500px] max-h-[300px] flex-col"
             >
-                <h3 class="text-5xl self-center p-10 font-bold text-gray-900">
+                <h3 class="text-7xl py-10 font-bold text-gray-900">
                     Заказ № {{ order.id }}
                 </h3>
                 <div class="flex">
-                    <p class="text-gray text-lg mr-6">Статус заказа:</p>
-                    <p class="text-blue-500 text-lg">{{ order.status }}</p>
+                    <p class="text-gray-700 text-2xl mr-4">Статус заказа:</p>
+                    <p class="text-2xl">{{ order.status }}</p>
                 </div>
                 <div class="flex">
-                    <p class="text-gray text-lg mr-6">Дата:</p>
-                    <p class="text-blue-500 text-lg">{{ order.created_at }}</p>
+                    <p class="text-gray text-2xl mr-4">Дата:</p>
+                    <p class="text-2xl">{{ order.created_at }}</p>
                 </div>
                 <div class="flex">
-                    <p class="text-gray text-lg mr-6">Итоговая цена:</p>
-                    <p class="text-blue-500 text-lg">
-                        {{ order.total_price }} BYN
-                    </p>
+                    <p class="text-gray text-2xl mr-4">Итоговая стоимость:</p>
+                    <p class="text-2xl">{{ order.total_price }} BYN</p>
                 </div>
             </div>
             <div class="border-2 rounded-lg">
-                <p class="text-center py-4 border-b-2 text-3xl mb-6 font-bold">
+                <p class="text-center py-4 border-b-2 text-4xl mb-6 font-bold">
                     Приобретённые детали
                 </p>
                 <div class="px-12 h-[500px] overflow-y-auto">
