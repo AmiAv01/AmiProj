e13b823288eabd3aca73c3fa2b7c57c08a2bfb94
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-25 17:13:01 +0300

    feat: add news edit form component

---
+++ resources/js/Shared/Forms/NewsEditForm.vue
@@ -0,0 +1,95 @@
+<template>
+    <modal
+        :show="isShow"
+        :title="`Редактирование новости`"
+        @closeModal="closeModal"
+    >
+        <div class="w-full">
+            <form
+                class="bg-white shadow-md flex flex-col rounded px-8 pt-6 pb-8 mb-4"
+            >
+                <div class="mb-4">
+                    <label
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                        for="username"
+                    >
+                        Название
+                    </label>
+                    <input
+                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        type="text"
+                        v-model="currentTitle"
+                        placeholder="Ввести название"
+                    />
+                </div>
+                <div class="mb-6">
+                    <label
+                        class="block text-gray-700 text-sm font-bold mb-2"
+                        for="password"
+                    >
+                        Описание
+                    </label>
+                    <textarea
+                        v-model="currentDescription"
+                        placeholder="Ввести описание"
+                        class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    />
+                </div>
+                <button
+                    @click.prevent="editPost"
+                    class="bg-green-700 rounded-lg text-white px-5 py-2.5 mx-auto text-lg mt-4"
+                >
+                    Изменить
+                </button>
+            </form>
+        </div>
+    </modal>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+    data() {
+        return {
+            currentTitle: this.title,
+            currentDescription: this.description,
+        };
+    },
+    props: {
+        title: {
+            type: String,
+            default: "",
+        },
+        description: {
+            type: String,
+            default: "",
+        },
+        isShow: {
+            type: Boolean,
+            default: false,
+        },
+        postId: {
+            type: Number,
+            default: 0,
+        },
+    },
+
+    methods: {
+        closeModal() {
+            this.$emit("closeModal");
+        },
+        editPost() {
+            axios
+                .put(`/admin/news/${this.postId}`, {
+                    title: this.title,
+                    description: this.description,
+                })
+                .then((res) => console.log(res))
+                .catch((err) => console.log(err));
+        },
+    },
+    created() {
+        console.log(this.postId);
+    },
+};
+</script>
