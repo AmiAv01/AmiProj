658204c48e65f2eb73234533a6af20e5fbaffc17
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-25 17:00:43 +0300

    feat: add searching for news

--- app/Services/NewsService.php
+++ app/Services/NewsService.php
@@ -8,12 +8,17 @@ class NewsService
 {
     public function getAll()
     {
-        return News::all();
+        return News::join('user', 'news.author', '=', 'user.id')->select('news.id', 'news.title', 'news.date', 'news.description', 'user.name')->paginate(12);
     }
 
     public function destroy()
     {
 
+    }
+
+    public function getBySearching(string $search)
+    {
+        return News::where('title', 'like', "%$search%")->paginate(12)->withQueryString();
     }
 
     public function show()
@@ -22,5 +27,6 @@ class NewsService
 
     public function update()
     {
+
     }
 }
