d7cbdbb45a5ab9f97c9554170c02ac88bbd53856
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-07-25 17:02:17 +0300

    feat: change code styling

--- resources/js/Components/MenuButton.vue
+++ resources/js/Components/MenuButton.vue
@@ -1,7 +1,7 @@
 <template>
     <inertia-link
         :href="`${href}`"
-        :class="`flex items-center text-white bg-green-700  hover:bg-green-800  font-medium rounded-lg  ${attributes} text-center dark:bg-green-600  `"
+        :class="`flex items-center text-white bg-green-700  hover:bg-green-800  font-medium rounded-lg   text-center dark:bg-green-600  ${attributes}`"
     >
         <slot />
     </inertia-link>
