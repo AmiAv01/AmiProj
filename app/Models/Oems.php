f6202465285fdd87dcfea052b85eff46dc4af19d
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-08-07 15:09:15 +0300

    feat: add search function by scout

--- app/Models/Oems.php
+++ app/Models/Oems.php
@@ -4,17 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Oems extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
-    public static function scopeSearch($query, $keywords)
+    public function toSearchableArray()
     {
-        return $query->when($keywords, function ($query, $keywords) {
-            $query->selectRaw('*, match(dt_invoice, dt_oem, dt_parent, fr_code) against(? IN boolean mode)', [$keywords])->whereRaw('match(dt_invoice, dt_oem, dt_parent, fr_code) against (? IN boolean mode)', [$keywords]);
-        }, function ($query) {
-            $query->latest();
-        });
+        return [
+            'dt_invoice' => $this->dt_invoice,
+            'dt_oem' => $this->dt_oem,
+        ];
     }
 }
