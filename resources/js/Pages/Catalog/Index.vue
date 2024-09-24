936bff75d780f93d268e56d897e4d3f0a48f1f1d
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-09-24 14:26:46 +0300

    fix: change the appearance of the components

--- resources/js/Pages/Catalog/Index.vue
+++ resources/js/Pages/Catalog/Index.vue
@@ -3,30 +3,35 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"  stroke="#FFFFFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>
     </push>
     <layout :title="title">
-        <div class="bg-white flex ml-10 py-24 sm:py-32">
-            <brand-selector
-                :categories="categories.brands"
-                :clientBrands="clientBrands"
-            />
-            <div class="ml-[150px] max-w-7xl">
-                <div class="mx-auto max-w-2xl lg:mx-0">
-                    <p class="text-5xl font-bold tracking-tight text-gray-900">
+        <div class="bg-white flex justify-around py-12 sm:py-12">
+            <div>
+                <div class="mx-auto max-w-xl ">
+                    <p class="text-5xl font-bold tracking-tight text-gray-900 mb-10">
                         {{ title }}
                     </p>
-                    <div
-                        class="mt-12 grid grid-cols-1 gap-x-6 gap-y-10 border-t w-[1300px] border-gray-200"
+                    <div v-if="details.data"
+                        class=" grid grid-cols-1 gap-x-4 gap-y-2 w-[1200px]  border-gray-200"
                     >
                         <catalog-card
                             @showPush="showModal"
-                            class="grid grid-cols-7 w-full pb-6 border-b border-gray-100 group mt-12"
+
                             v-for="detail in details.data"
                             :key="detail.dt_id"
                             :detail="detail"
                         />
                     </div>
+                    <div v-else
+                         class="mt-12 grid grid-cols-1 gap-x-6 gap-y-10 border-t w-[1300px] border-gray-200"
+                    >
+                        <p class="text-center mt-12 text-5xl text-gray-500">По данному запросу запачстей не найдено</p>
+                    </div>
                 </div>
                 <pagination :links="details.links"  />
             </div>
+            <brand-selector
+                :categories="categories.brands"
+                :clientBrands="clientBrands"
+            />
         </div>
     </layout>
 </template>
