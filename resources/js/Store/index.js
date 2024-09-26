e9729b12b08f5a8bb8587154d072e0e9e8a3805c
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-09-26 13:00:56 +0300

    feat: added Maps for displaying categories and their links

---
+++ resources/js/Store/index.js
@@ -0,0 +1,20 @@
+export let starterParts = new Map([["Вилки стартера", "/catalog/generator_parts/rectifier"],
+    ["Втулки стартера", "/catalog/starter_parts/sleeve"], ["Втягивающие реле", "/catalog/starter_parts/relay"],
+    ["Детали втягивающего реле", "/catalog/starter_parts/relay_parts"],
+    ["Детали привода стартера", "/catalog/starter_parts/unit_parts"], ["Крышки и головы", "/catalog/starter_parts/cover"],
+    ["Приводы", "/catalog/starter_parts/unit"], ["Прочее для стартера", "/catalog/starter_parts/other"],
+    ["Редукторы и компоненты", "/catalog/starter_parts/reducer"], ["Ремкомплекты", "/catalog/starter_parts/repair"],
+    ["Сальники стартера", "/catalog/starter_parts/glands"], ["Статоры", "/catalog/starter_parts/stator"],
+    ["Щётки стартера", "/catalog/starter_parts/brush"], ["Щёткодержатели стартера", "/catalog/starter_parts/holder"],
+    ["Якоря", "/catalog/starter_parts/anchor"]]);
+
+export let generatorParts = new Map([["Втулки генератора", "/catalog/generator_parts/sleeve"],
+    ["Вакуумные насосы", "/catalog/generator_parts/pump"], ["Выпрямители", "/catalog/generator_parts/rectifier"],
+    ["Диоды и триоды", "/catalog/generator_parts/diode"], ["Изоляторы", "/catalog/generator_parts/insulator"],
+    ["Крышки", "/catalog/generator_parts/cover"], ["Регуляторы", "/catalog/generator_parts/regulator"],
+    ["Роторы", "/catalog/generator_parts/rotor"], ["Терминалы", "/catalog/generator_parts/terminal"],
+    ["Сальники", "/catalog/generator_parts/gland"], ["Статорные обмотки", "/catalog/generator_parts/winding"],
+    ["Шкивы", "/catalog/generator_parts/pulley"], ["Щётки генератора", "/catalog/generator_parts/brush"],
+    ["Щёточные узлы", "/catalog/generator_parts/holder"]]);
+
+export let otherParts = new Map(['startes', "/catalog/starters", ''] )
