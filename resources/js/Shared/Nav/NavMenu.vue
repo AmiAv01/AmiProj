5e60851c9da3f086cf268f17cf072b40e2f3da04
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-09-26 13:04:28 +0300

    fix: added vFor loop to simplify the component

--- resources/js/Shared/Nav/NavMenu.vue
+++ resources/js/Shared/Nav/NavMenu.vue
@@ -43,65 +43,9 @@
                 <ul
                     class="absolute top-[100%] left-0 flex-col w-full justify-between bg-slate-900 rounded-[15px] border-b p-3 hidden z-50 group-hover/main:grid grid-cols-4 gap-4"
                 >
-                    <MenuItem
-                        value="Вилки стартера"
-                        link="/catalog/starter_parts/fork"
-                    />
-                    <MenuItem
-                        value="Втулки стартера"
-                        link="/catalog/starter_parts/sleeve"
-                    />
-                    <MenuItem
-                        value="Втягивающие реле"
-                        link="/catalog/starter_parts/relay"
-                    />
-                    <MenuItem
-                        value="Детали втягивающего реле"
-                        link="/catalog/starter_parts/relay_parts"
-                    />
-                    <MenuItem
-                        value="Детали привода стартера"
-                        link="/catalog/starter_parts/unit_parts"
-                    />
-                    <MenuItem
-                        value="Крышки и головы"
-                        link="/catalog/starter_parts/cover"
-                    />
-                    <MenuItem
-                        value="Приводы"
-                        link="/catalog/starter_parts/unit"
-                    />
-                    <MenuItem
-                        value="Прочее для стартера"
-                        link="/catalog/starter_parts/other"
-                    />
-                    <MenuItem
-                        value="Редукторы и компоненты"
-                        link="/catalog/starter_parts/reducer"
-                    />
-                    <MenuItem
-                        value="Ремкомплекты"
-                        link="/catalog/starter_parts/repair"
-                    />
-                    <MenuItem
-                        value="Сальники стартера"
-                        link="/catalog/starter_parts/glands"
-                    />
-                    <MenuItem
-                        value="Статоры"
-                        link="/catalog/starter_parts/stator"
-                    />
-                    <MenuItem
-                        value="Щётки стартера"
-                        link="/catalog/starter_parts/brush"
-                    />
-                    <MenuItem
-                        value="Щёткодержатели стартера"
-                        link="/catalog/starter_parts/holder"
-                    />
-                    <MenuItem
-                        value="Якоря"
-                        link="/catalog/starter_parts/anchor"
+                    <MenuItem v-for="[title, link] in starterParts"
+                              :value="`${title}`"
+                              :link="`${link}`"
                     />
                 </ul>
             </li>
@@ -127,61 +71,9 @@
                 <ul
                     class="absolute top-[100%] left-0 flex-col w-full justify-between bg-slate-900 rounded-[15px] border-b p-3 hidden z-50 group-hover/main:grid grid-cols-4 gap-4"
                 >
-                    <MenuItem
-                        value="Втулки генератора"
-                        link="/catalog/generator_parts/sleeve"
-                    />
-                    <MenuItem
-                        value="Вакуумные насосы"
-                        link="/catalog/generator_parts/pump"
-                    />
-                    <MenuItem
-                        value="Выпрямители"
-                        link="/catalog/generator_parts/rectifier"
-                    />
-                    <MenuItem
-                        value="Диоды и триоды"
-                        link="/catalog/generator_parts/diode"
-                    />
-                    <MenuItem
-                        value="Изоляторы"
-                        link="/catalog/generator_parts/insulator"
-                    />
-                    <MenuItem
-                        value="Крышки"
-                        link="/catalog/generator_parts/cover"
-                    />
-                    <MenuItem
-                        value="Регуляторы"
-                        link="/catalog/generator_parts/regulator"
-                    />
-                    <MenuItem
-                        value="Роторы"
-                        link="/catalog/generator_parts/rotor"
-                    />
-                    <MenuItem
-                        value="Терминалы"
-                        link="/catalog/generator_parts/terminal"
-                    />
-                    <MenuItem
-                        value="Сальники"
-                        link="/catalog/generator_parts/gland"
-                    />
-                    <MenuItem
-                        value="Статорные обмотки"
-                        link="/catalog/generator_parts/winding"
-                    />
-                    <MenuItem
-                        value="Шкивы"
-                        link="/catalog/generator_parts/pulley"
-                    />
-                    <MenuItem
-                        value="Щётки генератора"
-                        link="/catalog/generator_parts/brush"
-                    />
-                    <MenuItem
-                        value="Щёточные узлы"
-                        link="/catalog/generator_parts/holder"
+                    <MenuItem v-for="[title, link] in generatorParts"
+                        :value="`${title}`"
+                        :link="`${link}`"
                     />
                 </ul>
             </li>
@@ -209,4 +101,5 @@
 
 <script setup>
 import MenuItem from "@/Shared/Nav/NavMenuItem.vue";
+import {starterParts, generatorParts} from "@/Store/index.js";
 </script>
