7a04dc430eef29b27dfc17996fd13c19cd320cb8
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-05 13:07:45 +0300

    feat: add LoginRequest for params

--- app/Http/Controllers/Admin/AdminAuthController.php
+++ app/Http/Controllers/Admin/AdminAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
@@ -14,7 +15,7 @@ class AdminAuthController extends Controller
         return Inertia::render('Admin/Auth/Login');
     }
 
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'isAdmin' => true])) {
             return redirect()->route('admin.dashboard');
