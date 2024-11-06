89145e474c765c11e59ca84affc4fb8819126662
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-06 11:10:35 +0300

    fix: fix bugs and add actionTitle param

--- resources/js/Shared/Forms/NewsEditForm.vue
+++ resources/js/Shared/Forms/NewsEditForm.vue
@@ -6,7 +6,7 @@
     >
         <div class="w-full">
             <form
-                class="bg-white shadow-md flex flex-col rounded px-8 pt-6 pb-8 mb-4"
+                class="bg-white shadow-md flex flex-col rounded px-8 pt-6 pb-8 "
             >
                 <div class="mb-4">
                     <label
@@ -32,14 +32,14 @@
                     <textarea
                         v-model="currentDescription"
                         placeholder="Ввести описание"
-                        class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        class="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                     />
                 </div>
                 <button
-                    @click.prevent="editPost"
+                    @click.prevent = "editPost"
                     class="bg-green-700 rounded-lg text-white px-5 py-2.5 mx-auto text-lg mt-4"
                 >
-                    Изменить
+                    {{ actionTitle }}
                 </button>
             </form>
         </div>
@@ -72,22 +72,30 @@ export default {
             type: Number,
             default: 0,
         },
+        actionTitle: {
+            type: String,
+            default: '',
+        }
     },
-
+    emits: {
+        closeModal: false
+    },
     methods: {
         closeModal() {
             this.$emit("closeModal");
         },
         editPost() {
+            console.log(this.postId)
             axios
-                .put(`/admin/news/${this.postId}`, {
-                    title: this.title,
-                    description: this.description,
+                .patch(`/admin/news/${this.postId}`, {
+                    title: this.currentTitle,
+                    description: this.currentDescription,
                 })
                 .then((res) => console.log(res))
                 .catch((err) => console.log(err));
         },
     },
+
     created() {
         console.log(this.postId);
     },
