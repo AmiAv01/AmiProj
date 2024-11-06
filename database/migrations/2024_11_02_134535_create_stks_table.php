8c043ce3621a2341460c9b3aa76f776e74808158
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-06 11:28:53 +0300

    feat: add stk migration

---
+++ database/migrations/2024_11_02_134535_create_stks_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stk', function (Blueprint $table) {
+            $table->id('code');
+            $table->text('ostc', 10);
+            $table->text('ost', 10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stks');
+    }
+};
