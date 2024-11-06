286a9262d64ad04644d78f90f2dccce2d2bf5eaf
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-06 11:21:27 +0300

    feat: add store and destroy methods and rewrite update method

--- app/Http/Controllers/Admin/NewsAdminController.php
+++ app/Http/Controllers/Admin/NewsAdminController.php
@@ -3,27 +3,37 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\NewsFormRequest;
 use App\Services\NewsService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class NewsAdminController extends Controller
 {
     public function __construct(protected NewsService $newsService)
-    {
+    {}
 
-    }
-
-    public function index()
+    public function index():Response
     {
         return Inertia::render('Admin/News/NewsList', ['news' => $this->newsService->getAll()]);
     }
 
-    public function update(Request $request, $post)
-    {
-        Log::info(strval($request));
+    public function store(NewsFormRequest $request){
+        $request->validated();
+        return $this->newsService->store($request->all(), auth()->id());
+    }
 
-        return $request;
+    public function update(NewsFormRequest $request,int $post)
+    {
+
+        $request->validated();
+        Log::info($request);
+        return $this->newsService->update($request->all(), $post);
+    }
+
+    public function destroy(int $post){
+        return $this->newsService->destroy($post);
     }
 }
