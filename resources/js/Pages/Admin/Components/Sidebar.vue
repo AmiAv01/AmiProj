84e2fafffb1b80d4faf61d0b70e9718c2b9bf72f
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-24 11:28:06 +0300

    fix: add user field

--- resources/js/Pages/Admin/Components/Sidebar.vue
+++ resources/js/Pages/Admin/Components/Sidebar.vue
@@ -55,6 +55,13 @@
                         >Новости</admin-button
                     >
                 </li>
+                <li>
+                    <admin-button
+                        :href="`admin.users.index`"
+                        class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                    >Пользователи</admin-button>
+                    >
+            </li>
             </ul>
         </div>
     </aside>
