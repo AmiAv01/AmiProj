cd95e58aa7d3f2b5f2c4eb2d02b4254c0afa8724
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-24 11:29:20 +0300

    feat: add user card

---
+++ resources/js/Pages/Admin/User/UserCard.vue
@@ -0,0 +1,57 @@
+<template>
+    <admin-layout>
+        <div class="flex flex-col p-10 w-[75%] mx-auto justify-around">
+            <div
+                class="flex rounded-[15px] pb-10 w-[500px] max-h-[300px] flex-col"
+            >
+                <h3 class="text-7xl py-10 font-bold text-gray-900">
+                    {{ user.name }}
+                </h3>
+                <div class="flex">
+                    <p class="text-gray-700 text-2xl mr-4">Email:</p>
+                    <p class="text-2xl">{{ user.email }}</p>
+                </div>
+                <div class="flex">
+                    <p class="text-gray-700 text-2xl mr-4">Админ:</p>
+                    <p class="text-2xl">{{ (user.isAdmin === 1) ? "Да" : "Нет" }}</p>
+                </div>
+            </div>
+            <div class="border-2 mt-10 rounded-lg">
+                <p class="text-center py-4 border-b-2 text-4xl  font-bold">
+                    Заказы
+                </p>
+                <order-list :orders="orders"/>
+            </div>
+            <div class="border-2 mt-10 rounded-lg">
+                <p class="text-center py-4 border-b-2 text-4xl  font-bold">
+                    Корзина
+                </p>
+                <detail-list :details="cart"/>
+            </div>
+        </div>
+    </admin-layout>
+</template>
+
+<script>
+import UserOrderItems from "@/Pages/Admin/User/UserOrderItems.vue";
+export default {
+    components: {
+        "order-list": UserOrderItems,
+    },
+    created() {
+        console.log(this.order);
+        console.log(this.details);
+    },
+};
+</script>
+
+<script setup>
+import AdminLayout from "@/Pages/Admin/Components/AdminLayout.vue";
+import DetailList from "@/Shared/DetailList.vue";
+
+defineProps({
+    orders: Array,
+    cart: Array,
+    user: Object,
+});
+</script>
