fd21715eb36708a4a57dc4e923f1f20a6507ae93
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-24 11:02:05 +0300

    feat: add UserService

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,12 @@
 namespace App\Providers;
 
 use App\Models\Firm;
+use App\Models\User;
 use App\Services\CartService;
 use App\Services\DetailService;
 use App\Services\NewsService;
 use App\Services\OrderService;
+use App\Services\UserService;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 use Inertia\ResponseFactory;
@@ -22,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(OrderService::class);
         $this->app->bind(NewsService::class);
         $this->app->bind(DetailService::class);
+        $this->app->bind(UserService::class);
     }
 
     /**
