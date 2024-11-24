6dfa1b8c3d52bda24f413ddaf745905604ed215b
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-24 11:28:40 +0300

    feat: add admin user controller

---
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\NewsFormRequest;
+use App\Services\CartService;
+use App\Services\NewsService;
+use App\Services\OrderService;
+use App\Services\UserService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AdminUserController extends Controller
+{
+    public function __construct(protected UserService $userService, protected CartService $cartService, protected OrderService $orderService)
+    {
+    }
+
+    public function index(): Response
+    {
+        return Inertia::render('Admin/User/UserList', ['users' => $this->userService->getAll(12)]);
+    }
+
+    public function show(int $user): Response
+    {
+        return Inertia::render('Admin/User/UserCard', ['user' => $this->userService->getById($user), 'cart' => $this->cartService->getCartItems($user),
+            'orders' => $this->orderService->getByUserId($user)]);
+    }
+
+    public function destroy(int $post)
+    {
+        return $this->newsService->destroy($post);
+    }
+}
