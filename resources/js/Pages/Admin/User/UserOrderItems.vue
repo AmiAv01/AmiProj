a0c4f1c0297671e4d560cf0193c4ecc9960f0bb8
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-11-24 11:32:27 +0300

    feat: add user item

---
+++ resources/js/Pages/Admin/User/UserOrderItems.vue
@@ -0,0 +1,64 @@
+<template>
+        <section class=" w-full overflow-y-auto">
+            <div class="w-full">
+                <!-- Start coding here -->
+                <div
+                    class="bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden"
+                >
+                    <div class="overflow-x-auto">
+                        <table
+                            class="w-full text-sm text-left text-gray-500 dark:text-gray-400"
+                        >
+                            <thead
+                                class="text-xs text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+                            >
+                            <tr>
+                                <th scope="col" class="px-4 py-3">#</th>
+                                <th scope="col" class="px-4 py-3">Стоимость</th>
+                                <th scope="col" class="px-4 py-3">
+                                    Статус
+                                </th>
+                                <th scope="col" class="px-4 py-3">
+                                    Имя
+                                </th>
+                                <th scope="col" class="px-4 py-3">Email</th>
+                                <th scope="col" class="px-4 py-3">Дата</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <order-item
+                                v-for="(order) in orders"
+                                :key="`${order.id}`"
+                                :order="order"/>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+</template>
+
+<script>
+import Search from "@/Pages/Admin/Search.vue";
+import OrderItem from "@/Pages/Admin/Orders/OrderItem.vue";
+export default {
+    data() {
+        return {
+            searchOrders: this.orders,
+        };
+    },
+    components: {
+        "order-item": OrderItem,
+        search: Search,
+    },
+    props: {
+        orders: {
+            type: Array,
+            default: [],
+        },
+    },
+    created() {
+        console.log(this.orders);
+    },
+};
+</script>
