fa8bc6b3f1f50414b2f104d46b17dd22b491b5ce
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-02 12:21:23 +0300

    fix: delete toSearchableArray method

--- app/Models/Oems.php
+++ app/Models/Oems.php
@@ -4,17 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Laravel\Scout\Searchable;
 
 class Oems extends Model
 {
-    use HasFactory, Searchable;
+    use HasFactory;
 
-    public function toSearchableArray()
-    {
-        return [
-            'dt_invoice' => $this->dt_invoice,
-            'dt_oem' => $this->dt_oem,
-        ];
-    }
 }
