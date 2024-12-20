0929131e98ac13bff7c68a6ab25ac723f90af194
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-20 17:53:39 +0300

    feat: add code scope

--- app/Models/Oems.php
+++ app/Models/Oems.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Oems extends Model
 {
     use HasFactory;
 
+    public function scopeOfCode(Builder $query, string $code):void
+    {
+        $query->where('dt_invoice', '=', $code)->orWhere('dt_oem', '=', $code);
+    }
 }
