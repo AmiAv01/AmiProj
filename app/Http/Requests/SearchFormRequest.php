1b7ad55a93065968eb88ac9e99737c379482f324
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-20 17:48:36 +0300

    feat: add SearchFormRequest

---
+++ app/Http/Requests/SearchFormRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchFormRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'searchQ' => 'string|nullable|max:255'
+        ];
+    }
+
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
