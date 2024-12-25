5c5e0dc068a03162875c59ac07f3c3ca0152b84b
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-25 08:39:20 +0300

    feat: add scope

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -17,4 +18,9 @@ class Cart extends Model
     {
         return $this->hasMany(CartItem::class, 'cart_id', 'cart_id');
     }
+
+    public function scopeUser(Builder $query, int $userId):void{
+        $query->where('user_id', '=', $userId);
+    }
+
 }
