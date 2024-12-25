cad1f0dae7a0eabf656bd2bbf2b4f2b1701a6488
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-25 08:39:47 +0300

    feat: add DTO and request

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Cart;
 
+use App\DTO\CartDTO;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CartFormRequest;
 use App\Services\CartService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -12,30 +14,27 @@ use Inertia\Response;
 class CartController extends Controller
 {
     public function __construct(protected CartService $cartService)
-    {
+    {}
 
-    }
-
-    public function index(Request $request): Response
+    public function index(): Response
     {
         return Inertia::render('Cart/Cart', ['items' => $this->cartService->getCartItems(auth()->id())]);
     }
 
-    public function store(Request $request)
+    public function store(CartFormRequest $request)
     {
-        return $this->cartService->addToCart(auth()->id(), $request->id);
+        return $this->cartService->addToCart(auth()->id(), new CartDTO($request->validated('id'), $request->validated('quantity')));
     }
 
-    public function update(Request $request, int $id): array
+    public function update(CartFormRequest $request, int $id): array
     {
-        $this->cartService->updateQuantity(auth()->id(), $id, $request->quantity);
+        $this->cartService->updateQuantity(auth()->id(), new CartDTO($id, $request->validated('quantity')));
         return ['items' => $this->cartService->getCartItems(auth()->id())];
     }
 
     public function destroy(int $id): array
     {
         $this->cartService->deleteProductFromCart(auth()->id(), $id);
-
         return ['items' => $this->cartService->getCartItems(auth()->id())];
     }
 }
