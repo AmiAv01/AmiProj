ea102c7d23949b43952aa389621d4164750b9d74
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-25 08:42:03 +0300

    feat: add DTO

--- app/Services/NewsService.php
+++ app/Services/NewsService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\DTO\NewsPostDTO;
 use App\Http\Requests\NewsFormRequest;
 use App\Models\News;
 use Carbon\Carbon;
@@ -15,10 +16,9 @@ final class NewsService
         return News::join('user', 'news.author', '=', 'user.id')->select(['news.id', 'news.title', 'news.date', 'news.description', 'user.name'])->paginate($perPage);
     }
 
-    public function store(array $request, int $adminId):News
+    public function store(NewsPostDTO $dto, int $adminId):News
     {
-        Log::info($adminId);
-        return News::create(['title' => $request['title'], 'date' => Carbon::now() ,'description' => $request['description'], 'author' => $adminId]);
+        return News::create(['title' => $dto->title, 'date' => $dto->dateTime ,'description' => $dto->description, 'author' => $adminId]);
     }
 
     public function getBySearching(string $search)
@@ -26,10 +26,10 @@ final class NewsService
         return News::where('title', 'like', "%$search%")->paginate(12)->withQueryString();
     }
 
-    public function update(array $request, int $id):bool
+    public function update(NewsPostDTO $dto, int $id):bool
     {
         $news = News::find($id);
-        return $news->update(['title' => $request['title'], 'description' => $request['description']]);
+        return $news->update(['title' => $dto->title, 'description' => $dto->description]);
     }
 
     public function destroy(int $id):bool
