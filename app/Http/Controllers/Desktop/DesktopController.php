1ca4365c586be9855577588c516b467f6855ae38
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-25 08:40:40 +0300

    feat: add return value type

--- app/Http/Controllers/Desktop/DesktopController.php
+++ app/Http/Controllers/Desktop/DesktopController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\Desktop;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class DesktopController extends Controller
 {
-    public function index(){
+    public function index(): Response{
         return Inertia::render('Desktop/Index');
     }
 }
