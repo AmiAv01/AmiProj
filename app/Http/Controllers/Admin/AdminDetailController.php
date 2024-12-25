2e49bb92a4358581f8c1a6c2e72d91abe666a57b
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-25 08:37:59 +0300

    feat: add return value and params type

--- app/Http/Controllers/Admin/AdminDetailController.php
+++ app/Http/Controllers/Admin/AdminDetailController.php
@@ -8,20 +8,20 @@ use App\Models\Firm;
 use App\Services\DetailService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Inertia\Response;
+
 
 class AdminDetailController extends Controller
 {
     public function __construct(protected DetailService $detailService)
-    {
+    {}
 
-    }
-
-    public function index()
+    public function index(): Response
     {
         return Inertia::render('Admin/Detail/DetailList', ['details' => $this->detailService->getByBrand(12), 'brands' => Firm::all()]);
     }
 
-    public function delete(Request $request, $id)
+    public function delete(int $id)
     {
         Detail::where('dt_id', $id)->delete();
 
