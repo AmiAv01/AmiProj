e7d5343abb948e368de9e55889bea56d25cde3a6
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-25 08:42:39 +0300

    fix: delete return params

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use App\Services\DetailService;
-use Illuminate\Foundation\Application;
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -22,8 +22,6 @@ class UserController extends Controller
             'details' => $this->detailService->getAll(4),
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
-            'laravelVersion' => Application::VERSION,
-            'phpVersion' => PHP_VERSION,
         ]);
     }
 }
