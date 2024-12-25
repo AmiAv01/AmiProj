a23aeb7eb601ef7d97ba5f6d0052187eaa33ba28
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-25 08:37:36 +0300

    feat: add return value type and validation

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Inertia\Inertia;
+use Inertia\Response;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(): Response
     {
         return Inertia::render('Admin/Dashboard');
     }
