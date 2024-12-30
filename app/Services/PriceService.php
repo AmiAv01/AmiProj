ddbf981c05acff7ac6462ad75226ad1126dab3a1
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-30 19:45:32 +0300

    feat: add service

--- app/Services/PriceService.php
+++ app/Services/PriceService.php
@@ -2,28 +2,33 @@
 
 namespace App\Services;
 
-use App\Models\Detail;
 use App\Models\Price;
 use App\Models\User;
-use Illuminate\Support\Facades\Log;
+use Log;
+use Money\Money;
 
 final class PriceService{
-    public function getPrice(int $detailCode, int $userId): int | string {
+    public function getPrice(int $detailCode, int $userId): int | string | Money {
         $userFormula = User::where('id', '=' ,$userId)->value('formula');
         $price = $this->getPriceByType(strtolower(substr($userFormula, 0, 1)), $detailCode);
         Log::info($price);
         return  $this->getSpecialPrice($price, substr($userFormula, 1,1), substr($userFormula, 2, strpos($userFormula,"%")));
     }
 
-    public function getPriceByType(string $priceType, int $detailCode){
+    public function parsePrice(string $price){
+        return str_replace(',','.',$price);
+    }
+
+    public function getPriceByType(string $priceType, int $detailCode): string{
         return ($priceType === 'o') ? Price::where('code', '=', $detailCode)->value('opt') : Price::where('code', '=', $detailCode)->value('zakup');
     }
 
-    public function getSpecialPrice(int | string $price, string $sign, string $percent) {
+    public function getSpecialPrice(string $price, string $sign, string $percent): string
+    {
         if ($sign === ''){
             return $price;
         }
-        return  ($sign === '+') ? $price + $price * $percent/100 : $price - $price * $percent/100;
+        return  ($sign === '+') ? bcadd($price, bcmul($price, $percent/100)) : bcsub($price, bcmul($price, $percent/100));
     }
 }
 
