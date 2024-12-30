7b27bb9d5ef929c992cbe5d3117d83eaf829f142
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-30 14:25:31 +0300

    fix: update store method

--- app/Http/Controllers/Order/OrderController.php
+++ app/Http/Controllers/Order/OrderController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers\Order;
 
+use App\DTO\OrderDTO;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\OrderRequest;
+use App\Http\Resources\OrderResource;
 use App\Models\Order;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
@@ -13,21 +16,18 @@ use Inertia\Response;
 class OrderController extends Controller
 {
     public function __construct(protected OrderService $orderService)
-    {
-
-    }
+    {}
 
     public function index(): Response
     {
         $userId = auth()->check() ? auth()->user()->id : null;
-
         return Inertia::render('Order/OrderList', ['orders' => $this->orderService->getByUserId($userId)]);
     }
 
-    public function store(Request $request): Order
+    public function store(OrderRequest $request): Order
     {
-        $order = $this->orderService->createOrder(auth()->id(), $request['totalPrice']);
-        return $order;
+        $order = $this->orderService->createOrder(new OrderDTO($request->validated('totalPrice'), auth()->id()));
+        return OrderResource::make($order);
     }
 
     public function show(int $id): Response
