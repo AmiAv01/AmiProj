14d5795674c8df41fcd19c8d6cc498d6c8087e1c
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-30 19:40:09 +0300

    feat: add controller

---
+++ app/Http/Controllers/Admin/AdminCurrencyController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use app\ChangeCurrencyAction;
+use App\DTO\CurrencyDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CurrencyFormRequest;
+use App\Services\CurrencyService;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AdminCurrencyController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('Admin/Currency/Index');
+    }
+    public function update(CurrencyFormRequest $request, CurrencyService $currencyService)
+    {
+        return ['currency' => $currencyService->update(new CurrencyDTO($request->validated('currency')))];
+    }
+}
