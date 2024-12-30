132db972700476937fe0205ad8fad8140f080829
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-30 19:42:10 +0300

    feat: add component

---
+++ resources/js/Pages/Admin/Currency/Index.vue
@@ -0,0 +1,48 @@
+<template>
+    <admin-layout>
+        <p class="text-5xl font-bold tracking-tight text-gray-900 mb-10">
+            Курс валют
+        </p>
+        <div class="flex">
+            <input
+                class="shadow appearance-none border rounded w-[300px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                type="number"
+                step="0.1"
+                min="1"
+                v-model="currencyValue"
+                placeholder="Ввести курс валют"
+            />
+            <button
+                @click="changeCurrency"
+                class="bg-green-700 ml-2 rounded-lg text-white px-5 py-2.5 text-lg "
+            >
+                Изменить
+            </button>
+        </div>
+    </admin-layout>
+</template>
+
+<script>
+    import AdminLayout from "@/Pages/Admin/Components/AdminLayout.vue";
+
+    export default {
+        components: {AdminLayout},
+        data(){
+            return {
+                currencyValue: 0
+            }
+        },
+        methods: {
+            changeCurrency(){
+                axios.post(`/admin/currency`, {currency: this.currencyValue})
+                    .then(res => console.log(res.data))
+                    .catch(err => console.error(err))
+            }
+        }
+    }
+</script>
+
+
+<style scoped>
+
+</style>
