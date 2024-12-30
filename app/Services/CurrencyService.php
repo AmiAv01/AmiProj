9568dac27c50e0c1750ed54813b99e143bccd548
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2024-12-30 19:41:50 +0300

    feat: add service

---
+++ app/Services/CurrencyService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\DTO\CurrencyDTO;
+use App\Models\Currency;
+
+final class CurrencyService
+{
+    public function update(CurrencyDTO $dto):bool
+    {
+        return Currency::where('code', '=', 'EUR')->first()->update(['value'=>$dto->currency]);
+    }
+}
