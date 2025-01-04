7cace8949326822a49d6c9e27be1e1f5e068edbc
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-01-04 12:39:25 +0300

    feat: add pinia

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { createPinia } from 'pinia'
 import { Link } from '@inertiajs/inertia-vue3';
 import DetailList from "@/Shared/DetailList.vue";
 import UserLayout from "@/Shared/UserLayout.vue";
@@ -20,11 +21,12 @@ createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
     setup({ el, App, props, plugin }) {
+        const pinia = createPinia();
         const app = createApp({ render: () => h(App, props) });
         app.component('inertia-link', Link).component('detail-list', DetailList).component('layout', UserLayout).
             component('menu-button', MenuButton).component('cart-button', CartButton).component('pagination', Pagination)
             .component('modal', Modal).component('push', PushNotification);
-        return app.use(plugin)
+        return app.use(plugin).use(pinia)
         .use(ZiggyVue, Ziggy)
         .mount(el);
     },
