2c679fff6c8df3bf9171169e10f0b573ac75f68e
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-01-09 12:02:13 +0300

    fix: fix order info

--- resources/js/Pages/Order/OrderCard.vue
+++ resources/js/Pages/Order/OrderCard.vue
@@ -5,19 +5,19 @@
                 class="flex rounded-[15px] pb-10 w-[500px] max-h-[300px] flex-col"
             >
                 <h3 class="text-7xl py-10 font-bold text-gray-900">
-                    Заказ № {{ order.id }}
+                    Заказ № {{ order[0].id }}
                 </h3>
                 <div class="flex">
                     <p class="text-gray-700 text-2xl mr-4">Статус заказа:</p>
-                    <p class="text-2xl">{{ order.status }}</p>
+                    <p class="text-2xl">{{ order[0].status }}</p>
                 </div>
                 <div class="flex">
                     <p class="text-gray text-2xl mr-4">Дата:</p>
-                    <p class="text-2xl">{{ order.created_at }}</p>
+                    <p class="text-2xl">{{ order[0].created_at }}</p>
                 </div>
                 <div class="flex">
                     <p class="text-gray text-2xl mr-4">Итоговая стоимость:</p>
-                    <p class="text-2xl">{{ order.total_price }} BYN</p>
+                    <p class="text-2xl">{{ order[0].total_price }} BYN</p>
                 </div>
             </div>
             <div class="border-2 rounded-lg">
