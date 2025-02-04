9758cb787b5b8eede79d051a5186a4e6f6e9c97a
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-02-04 09:33:19 +0300

    fix: add crypt method

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -4,11 +4,12 @@ namespace App\Services;
 
 use App\DTO\CurrencyDTO;
 use App\Models\Currency;
+use Illuminate\Support\Facades\Crypt;
 
 final class CurrencyService
 {
     public function update(CurrencyDTO $dto):bool
     {
-        return Currency::where('code', '=', 'EUR')->first()->update(['value' => $dto->currency]);
+        return Currency::where('code', '=', 'EUR')->first()->update(['value' => Crypt::encrypt($dto->currency)]);
     }
 }
