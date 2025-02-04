5b45cb5202c35cf81354336bd7fea5c62bc1799b
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-02-04 09:43:58 +0300

    feat: add getting news data

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Services\DetailService;
 
+use App\Services\NewsService;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class UserController extends Controller
 {
-    public function __construct(protected DetailService $detailService)
+    public function __construct(protected DetailService $detailService, protected NewsService $newsService)
     {
 
     }
@@ -20,6 +21,7 @@ class UserController extends Controller
     {
         return Inertia::render('User/Index', [
             'details' => $this->detailService->getAll(4),
+            'posts' => $this->newsService->getAll(3),
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
         ]);
