46db77117a5086696b7c4b236af5745acf148d8e
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-02-04 09:38:15 +0300

    feat: change news component

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -7,7 +7,7 @@
         <section>
             <ProductList v-bind:details="details" />
             <LogoClouds/>
-            <News/>
+            <NewsList :posts="posts"/>
         </section>
     </layout>
 </template>
@@ -15,11 +15,15 @@
 <script setup>
 import LogoClouds from "./LogoClouds.vue";
 import ProductList from "./ProductList.vue";
-import News from "@/Shared/News.vue";
 import HeroSection from "@/Pages/User/HeroSection.vue";
+import NewsList from "@/Pages/News/NewsList.vue";
 
 const props = defineProps({
     details: Object,
+    posts: {
+        type: Array,
+        default: [],
+    }
 });
 
 </script>
