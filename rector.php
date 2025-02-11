d5ee504462e4178339725eac32716b0f6b157b57
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-02-11 12:52:19 +0300

    feat: add rector lib

---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/lang',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+        __DIR__ . '/vendor/spatie',
+        __DIR__ . '/vendor/tightenco/ziggy/src'
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withTypeCoverageLevel(0)
+    ->withDeadCodeLevel(0)
+    ->withCodeQualityLevel(0)
+    ->withRules([
+        Rector\Php84\Rector\Param\ExplicitNullableParamTypeRector::class,
+    ]);
