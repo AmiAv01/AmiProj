82eaccdefdbaeab69ac5257539540b12773c8ae0
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-02-28 13:12:56 +0300

    fix: add routes map to switch ziggy routes

---
+++ resources/js/Store/routes.js
@@ -0,0 +1,4 @@
+export const routes = new Map([['admin.login.post', '/admin/resource/login'],
+    ['profile.edit', '/profile'], ['logout', '/logout'], ['profile.destroy','/profile'],
+    ['password.update', '/password'], ['login', 'login'], ['admin.dashboard', '/admin/resource/dashboard'],
+    ['login', '/login'], ['password.request', '/forgot-password'], ['profile.update', '/profile']]);
