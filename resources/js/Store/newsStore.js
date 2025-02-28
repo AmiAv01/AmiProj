186c7a87d19f95a24b62ce9b8825a97fe53dc25e
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-02-28 13:13:42 +0300

    feat: add newsStore

---
+++ resources/js/Store/newsStore.js
@@ -0,0 +1,42 @@
+import {defineStore} from "pinia";
+import axios from "axios";
+
+export const useNewsStore = defineStore('news', {
+    state: () => ({ newsData: [] }),
+    actions: {
+        setNews(items){
+            this.newsData = items;
+            axios
+                .post(`/admin/resource/news/store`, {
+                    title,
+                    description,
+                })
+                .then((res) => {
+                    console.log(res.data)
+                    this.setNews(res.data.items)
+                })
+                .catch((err) => console.log(err));
+        },
+        deletePost(id){
+            axios.delete(`/admin/resource/news/${id}`)
+                .then(res => {
+                    console.log(res.data)
+                    this.setNews(res.data.items)
+                })
+                .catch(err => console.log(err))
+        },
+        editPost(id, title, description) {
+            console.log(id);
+            axios
+                .patch(`/admin/resource/news/${id}`, {
+                    title,
+                    description,
+                })
+                .then(res => {
+                    console.log(res.data)
+                    this.setNews(res.data.items)
+                })
+                .catch((err) => console.log(err));
+        }
+    }
+})
