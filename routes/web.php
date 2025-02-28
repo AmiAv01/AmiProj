65376e1c380beefd413aed58349dc9e793834ede
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-02-28 13:04:04 +0300

    fix: add resource to admin and delete header

--- routes/web.php
+++ routes/web.php
@@ -28,92 +28,85 @@ use App\Http\Controllers\SearchController;
 use App\Http\Controllers\User\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\RemoveHeader;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-Route::get('/', [UserController::class, 'index'])->name('home');
+Route::group(['middleware' => 'removeHeader'], function(){
+    Route::get('/', [UserController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['auth', 'verified', ''])->name('dashboard');
 
-Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+    Route::get('/news', [NewsController::class, 'index'])->name('news.index');
+    Route::get('/info', [InfoController::class, 'index'])->name('info.index');
+    Route::get('/desktop', [DesktopController::class, 'index'])->name('desktop.index');
 
-Route::get('/api/search', [SearchController::class, 'index']);
-
-Route::group(['prefix' => 'catalog'], (function (): void {
-    Route::get('/search', [CatalogSearchedController::class, 'index']);
     Route::get('/api/search', [SearchController::class, 'index']);
-    Route::get('/generators', [GeneratorController::class, 'index'])->name('generator.index');
-    Route::get('/starters', [StarterController::class, 'index'])->name('starter.index');
-    Route::get('/bearings', [BearingController::class, 'index'])->name('bearing.index');
-    Route::get('/starter_parts/{category?}', [StarterPartsController::class, 'index']);
-    Route::get('/generator_parts/{category}', [GeneratorPartsController::class, 'index']);
-    Route::get('/other', [OtherDetailsController::class, 'index']);
-    Route::get('/product/{id}', [ProductController::class, 'index'])->name('product.info');
-    Route::get('/starter_parts/product/{id}', [ProductController::class, 'index'])->name('product.info');
-    Route::get('/generator_parts/product/{id}', [ProductController::class, 'index'])->name('product.info');
-}));
 
-Route::get('/info', [InfoController::class, 'index'])->name('info.index');
-Route::get('/desktop', [DesktopController::class, 'index'])->name('desktop.index');
+    Route::group(['prefix' => 'catalog'], (function (): void {
+        Route::get('/search', [CatalogSearchedController::class, 'index'])->name('catalog-searched.index');
+        Route::get('/api/search', [SearchController::class, 'index'])->name('catalog-searched.index');
+        Route::get('/generators', [GeneratorController::class, 'index'])->name('generator.index');
+        Route::get('/starters', [StarterController::class, 'index'])->name('starter.index');
+        Route::get('/bearings', [BearingController::class, 'index'])->name('bearing.index');
+        Route::get('/starter_parts/{category?}', [StarterPartsController::class, 'index'])->name('starter-parts.index');
+        Route::get('/generator_parts/{category}', [GeneratorPartsController::class, 'index'])->name('generator-parts.index');
+        Route::get('/other', [OtherDetailsController::class, 'index'])->name('other.index');
+        Route::get('/product/{id}', [ProductController::class, 'index'])->name('product.info');
+        Route::get('/starter_parts/product/{id}', [ProductController::class, 'index'])->name('product.info');
+        Route::get('/generator_parts/product/{id}', [ProductController::class, 'index'])->name('product.info');
+    }));
 
-Route::middleware('auth')->resource('/cart', CartController::class)->only(['index', 'store', 'update', 'destroy']);
-Route::middleware('auth')->put('/clear', [ClearCartController::class, 'index'])->name('cart.clear');
-Route::middleware('auth')->resource('/order', OrderController::class)->only(['index', 'store', 'update', 'show']);
+    Route::middleware('auth')->resource('/cart', CartController::class)->only(['index', 'store', 'update', 'destroy']);
+    Route::middleware('auth')->put('/clear', [ClearCartController::class, 'index'])->name('cart.clear');
+    Route::middleware('auth')->resource('/order', OrderController::class)->only(['index', 'store', 'update', 'show']);
 
-Route::group(['prefix' => 'admin', 'middleware' => 'RedirectIfAdmin'], function (): void {
-    Route::get('login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
-    Route::post('login', [AdminAuthController::class, 'login'])->name('admin.login.post');
-    Route::get('logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+    Route::group(['prefix' => 'admin/resource', 'middleware' => 'RedirectIfAdmin'], function (): void {
+        Route::get('login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+        Route::post('login', [AdminAuthController::class, 'login'])->name('admin.login.post');
+        Route::get('logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+    });
+
+    Route::middleware(['auth', 'admin'])->prefix('admin/resource')->group(function (): void {
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
+        // details route
+        Route::get('/details', [AdminDetailController::class, 'index'])->name('admin.details.index');
+        Route::post('/details/store', [AdminDetailController::class, 'store'])->name('admin.details.store');
+        Route::put('/details/update/{id}', [AdminDetailController::class, 'update'])->name('admin.details.update');
+        Route::delete('/details/delete/{dt_id}', [AdminDetailController::class, 'delete'])->name('admin.details.delete');
+
+        // orders route
+        Route::get('/orders', [AdminOrderController::class, 'index'])->name('admin.orders.index');
+        Route::get('/orders/{order}', [AdminOrderController::class, 'show'])->name('admin.orders.show');
+
+        // news route
+        Route::get('/news', [AdminNewsController::class, 'index'])->name('admin.news.index');
+        Route::post('/news/store', [AdminNewsController::class, 'store'])->name('admin.news.store');
+        Route::patch('/news/{postId}', [AdminNewsController::class, 'update'])->name('admin.news.update');
+        Route::delete('/news/{postId}', [AdminNewsController::class, 'destroy'])->name('admin.news.delete');
+        Route::get('/api/search', [AdminSearchController::class, 'index'])->name('admin.search.index');
+
+        //users route
+        Route::get('/users', [AdminUserController::class, 'index'])->name('admin.users.index');
+        Route::get('/users/{user}', [AdminUserController::class, 'show'])->name('admin.users.show');
+        Route::put('users/{user}', [AdminUserController::class, 'update'])->name('admin.users.update');
+        Route::delete('/users',[AdminUserController::class, 'destroy'])->name('admin.users.delete');
+
+        //currency route
+        Route::get('currency', [AdminCurrencyController::class, 'index'])->name('admin.currency.index');
+        Route::post('currency', [AdminCurrencyController::class, 'update'])->name('admin.currency.update');
+
+        Route::put('/approve/{user}', [AdminApproveUserController::class, 'index'])->name('admin.approve.index');
+    });
+
+    require __DIR__.'/auth.php';
+
+    Route::middleware('auth')->group(function (): void {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
 });
 
-Route::middleware(['auth', 'admin'])->prefix('admin')->group(function (): void {
-    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
-
-    // details route
-    Route::get('/details', [AdminDetailController::class, 'index'])->name('admin.details.index');
-    Route::post('/details/store', [AdminDetailController::class, 'store'])->name('admin.details.store');
-    Route::put('/details/update/{id}', [AdminDetailController::class, 'update'])->name('admin.details.update');
-    Route::delete('/details/delete/{dt_id}', [AdminDetailController::class, 'delete'])->name('admin.details.delete');
-
-    // orders route
-    Route::get('/orders', [AdminOrderController::class, 'index'])->name('admin.orders.index');
-    Route::get('/orders/{order}', [AdminOrderController::class, 'show'])->name('admin.orders.show');
-
-    // news route
-    Route::get('/news', [AdminNewsController::class, 'index'])->name('admin.news.index');
-    Route::post('/news/store', [AdminNewsController::class, 'store'])->name('admin.news.store');
-    Route::patch('/news/{post}', [AdminNewsController::class, 'update'])->name('admin.news.update');
-    Route::delete('/news/{post}', [AdminNewsController::class, 'destroy'])->name('admin.news.delete');
-    Route::get('/api/search', [AdminSearchController::class, 'index'])->name('admin.search.index');
-
-    //users route
-    Route::get('/users', [AdminUserController::class, 'index'])->name('admin.users.index');
-    Route::get('/users/{user}', [AdminUserController::class, 'show'])->name('admin.users.show');
-    Route::put('users/{user}', [AdminUserController::class, 'update'])->name('admin.users.update');
-    Route::delete('/users',[AdminUserController::class, 'destroy'])->name('admin.users.delete');
-
-    //currency route
-    Route::get('currency', [AdminCurrencyController::class, 'index'])->name('admin.currency.index');
-    Route::post('currency', [AdminCurrencyController::class, 'update'])->name('admin.currency.update');
-
-    Route::put('/approve/{user}', [AdminApproveUserController::class, 'index'])->name('admin.approve.index');
-});
-
-require __DIR__.'/auth.php';
-
-Route::middleware('auth')->group(function (): void {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
