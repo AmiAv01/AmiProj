ae246da7d6942afffbe774fae95ac025ab36a326
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-03-12 13:56:50 +0300

    feat: add product page test

--- tests/Feature/ProductPageTest.php
+++ tests/Feature/ProductPageTest.php
@@ -1,21 +1,13 @@
 <?php
 
 
-test('return successful response when getting product', function () {
-    $response = $this->get('/catalog/product/');
+use App\Models\User;
 
+test('return successful response when getting product from auth user', function () {
+    $user = User::factory()->create([
+        'approved' => true
+    ]);
+    $response = $this->actingAs($user)->get("/catalog/product/CS1277");
     $response->assertStatus(200);
 });
 
-test('return successful response when getting product from starter parts', function () {
-    $response = $this->get('/catalog/starter_parts/product/{id}');
-
-    $response->assertStatus(200);
-});
-
-
-test('return successful response when getting product from starter parts', function () {
-    $response = $this->get('/catalog/starter_parts/product/{id}');
-
-    $response->assertStatus(200);
-});
