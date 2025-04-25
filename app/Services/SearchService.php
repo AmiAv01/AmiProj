cf31684b6c038a1ea2d75bad965cd35f6dcf6c53
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-04-25 09:46:52 +0300

    refactor: extract data processing logic into separate methods and another service

--- app/Services/SearchService.php
+++ app/Services/SearchService.php
@@ -3,44 +3,45 @@
 namespace App\Services;
 
 use App\DTO\SearchQueryDTO;
+use App\Exceptions\NoResultsFoundException;
+use App\Models\Detail;
 use App\Models\Oems;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 
 final class SearchService{
+
+    public function __construct(private OemService $oemService){}
     public function getBySearching(SearchQueryDTO $dto): array
     {
-        $detailsFromOems = Oems::where('dt_invoice', 'like', "$dto->searchQuery%")->orWhere('dt_oem', 'like', "$dto->searchQuery%")->get()->toArray();
-        $data = [];
-        $result = [];
-        $i = 0;
-        foreach ($detailsFromOems as $detail) {
-            $data[$i] = $this->getInfoAboutDetailFromOems($detail, $dto->searchQuery);
-            $i++;
+        $detailsFromOems = $this->oemService->findDetailsByQuery($dto->searchQuery);
+        if ($detailsFromOems->isEmpty()){
+            throw new NoResultsFoundException($dto->searchQuery);
         }
-        usort($data, function ($a, $b) {
-            return strcmp($a['dt_code'], $b['dt_code']);
-        });
-        foreach ($data as $item) {
-            $result[md5($item['dt_code'].$item['dt_firm'])] = $item;
-        }
-        return $result;
+        $processedDetails = $this->processDetails($detailsFromOems, $dto->searchQuery);
+        return $this->formatResults($processedDetails);
     }
-
-    public function getInfoAboutDetailFromOems(array | Oems $detail,string $searchQuery): array
-    {
-        $resultData = [];
-        $isStartWithOem = str_starts_with( $detail['dt_oem'], $searchQuery);
-        $resultData['dt_code'] = ($isStartWithOem) ? $detail['dt_oem'] : $detail['dt_invoice'];
-        $resultData['dt_firm'] = ($isStartWithOem) ? $detail['fr_code'] : $detail['dt_parent'];
-        $resultData['dt_typec'] = $detail['dt_typec'];
-        return $resultData;
-    }
-
     public function getBySearchingWithPagination(SearchQueryDTO $dto): LengthAwarePaginator
     {
         $details = $this->getBySearching($dto);
         return new LengthAwarePaginator($details, count($details), 10);
     }
+
+    private function processDetails(Collection $details, string $searchQuery): array{
+        return $details->map(function ($detail) use ($searchQuery) {
+            return $this->oemService->getInfoAboutDetailFromOems($detail, $searchQuery)->toArray();
+        })->toArray();
+    }
+
+    private function formatResults(array $details): array{
+        usort($details, fn($a, $b) => strcmp($a['dt_code'], $b['dt_code']));
+        $result = [];
+        foreach ($details as $detail) {
+            $result[md5($detail['dt_code'].$detail['dt_firm'])] = $detail;
+        }
+        return $result;
+    }
+
 }
 
