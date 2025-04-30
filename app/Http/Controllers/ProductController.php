962d12a19e3e2f954341b3d86b4a52d66a7404d4
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-04-30 10:10:56 +0300

    refactor: update ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,33 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\DetailService;
-use App\Services\ProductService;
-use App\Services\SearchService;
-use App\Services\PriceService;
-use Illuminate\Support\Facades\Log;
+use App\Services\Interface\ProductViewServiceInterface;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class ProductController extends Controller
 {
-    public function __construct(protected DetailService $detailService, protected SearchService $searchService, protected
-        PriceService $priceService, protected ProductService $productService)
-    {}
+    public function __construct(
+        private ProductViewServiceInterface $viewService,
+    ) {}
 
     public function index(string $id): Response
     {
-        $detail = $this->detailService->getByInvoice($id);
-        $analogs = $this->productService->getAnalogs($id);
-        if (!$detail->isEmpty()){
-            return (!auth()->check()) ? Inertia::render('Card/GuestCard', ['detail' => $detail[0], 'isEmpty' => false])
-                : Inertia::render('Card/Index', ['detail' => $detail[0], 'sameDetails' => $this->productService->getSameDetails($detail[0]->dt_typec, $id),
-                'analogs' => $analogs, 'isEmpty' => false, 'imageUrl' => $this->productService->getImageUrl($detail[0]->dt_foto),
-                'price' => $this->priceService->getPrice($detail[0]->dt_code, (auth()->user()->id))]);
-        }
-        $detailFromOems = $this->productService->getProductInfoFromOems($this->searchService, $id);
-        return (!auth()->check()) ? Inertia::render('Card/GuestCard', ['detail' => $detailFromOems, 'isEmpty' => false]) : Inertia::render('Card/Index', ['detail' => $detailFromOems,
-            'sameDetails' => $this->productService->getSameDetails($detailFromOems['dt_typec'], $id), 'analogs' => $analogs, 'cargoIds' => $this->productService->getCargoFromAnalogs($analogs), 'isEmpty' => true,
-            'imageUrl' => $this->productService->getImageUrl()]);
+        return Inertia::render(
+            $this->viewService->resolveViewName(),
+            $this->viewService->getViewDataForProduct($id)
+        );
     }
 }
