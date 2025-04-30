523514ef635b35425535d4e991dbd3d1f918fa33
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-04-30 10:00:51 +0300

    refactor: extract CompatiblePartsService from ProductService

---
+++ app/Services/Product/CompatiblePartsService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services\Product;
+
+use App\Models\AltCz;
+use App\Models\Detail;
+use App\Models\RozCz;
+
+class CompatiblePartsService
+{
+    public function getCompatibleParts(string $detailType, string $detailInvoice): array
+    {
+        return match($detailType){
+            'ГЕНЕРАТОР' => $this->getGeneratorCompatibleParts($detailInvoice),
+            'СТАРТЕР' => $this->getStarterCompatibleParts($detailInvoice),
+            default => [],
+        };
+    }
+    private function getGeneratorCompatibleParts(string $detailInvoice): array
+    {
+        $ids = AltCz::where('hcparts', $detailInvoice)->get()
+            ->reject(fn($item) => str_starts_with($item->dt_code, '-') || empty($item->dt_code))
+            ->pluck('dt_code')->unique()->values()->toArray();
+
+        return $this->getDetailsByCodes($ids);
+    }
+
+    private function getStarterCompatibleParts(string $detailInvoice): array
+    {
+        $ids = RozCz::where('hcparts', $detailInvoice)->get()
+            ->reject(fn($item) => str_starts_with($item->dt_code, '-') || empty($item->dt_code))
+            ->pluck('dt_code')->unique()->values()->toArray();
+        return $this->getDetailsByCodes($ids);
+    }
+
+    private function getDetailsByCodes(array $codes): array{
+        return Detail::whereIn('dt_cargo', $codes)
+            ->orWhereIn('dt_invoice', $codes)
+            ->orWhereIn('dt_oem', $codes)
+            ->select(['dt_invoice', 'dt_typec',  'dt_cargo', 'fr_code'])->get()->toArray();
+    }
+}
