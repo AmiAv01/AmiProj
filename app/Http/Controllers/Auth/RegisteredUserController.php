f728fa6ee1a05fcf45b0e22c079148d69cfc7cdc
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-04-30 10:12:57 +0300

    fix: delete CartService

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -6,13 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisteredUserRequest;
 use App\Models\User;
 use App\Providers\RouteServiceProvider;
-use App\Services\CartService;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -21,9 +17,6 @@ class RegisteredUserController extends Controller
     /**
      * Display the registration view.
      */
-    public function __construct(protected CartService $cartService)
-    {}
-
     public function create(): Response
     {
         return Inertia::render('Auth/Register');
@@ -43,7 +36,6 @@ class RegisteredUserController extends Controller
             'password' => Hash::make($request->validated('password')),
         ]);
 
-        $cart = $this->cartService->storeCart($user->id);
         event(new Registered($user));
 
 
