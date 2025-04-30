c450b32b8c4814cf9f481e7d6da8594e8895df3e
Golovchenko Yegor <78694320+Yglv@users.noreply.github.com>
2025-04-30 09:57:41 +0300

    feat: add exceptions

--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -3,17 +3,26 @@
 namespace App\Services;
 
 use App\DTO\CurrencyDTO;
+use App\Exceptions\CurrencyNotFoundException;
 use App\Models\Currency;
 use Illuminate\Support\Facades\Crypt;
 
 final class CurrencyService
 {
     public function getCurrency(): string{
-        return Crypt::decrypt(Currency::where('code', '=', 'EUR')->value('value'));
+        $currency = Currency::where('code', '=', 'EUR')->value('value');
+        if (!$currency) {
+            throw new CurrencyNotFoundException('EUR');
+        }
+        return Crypt::decrypt($currency);
     }
 
     public function update(CurrencyDTO $dto):bool
     {
+        $currency = Currency::where('code', '=', 'EUR')->first();
+        if (!$currency) {
+            throw new CurrencyNotFoundException('EUR');
+        }
         return Currency::where('code', '=', 'EUR')->first()->update(['value' => Crypt::encrypt($dto->currency)]);
     }
 }
