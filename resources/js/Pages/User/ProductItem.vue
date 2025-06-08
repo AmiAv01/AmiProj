7a0bfea9541f4ca22843e11f2f5ab1457dc5ca7b
AmiAv01 <amiavproject@gmail.com>
2025-06-08 14:33:25 +0300

    fix: change link

--- resources/js/Pages/User/ProductItem.vue
+++ resources/js/Pages/User/ProductItem.vue
@@ -15,7 +15,7 @@
         <div class="mt-4 flex justify-between">
             <div>
                 <h3 class="text-lg text-gray-700 font-bold">
-                    <a :href="detail">
+                    <a :href="catalog/product/${detail.dt_invoice}">
                                     <span
                                         aria-hidden="true"
                                         class="absolute inset-0"
