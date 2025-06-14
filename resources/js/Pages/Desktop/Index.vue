0796ff4e5c9ad221aabeb2065176d645080dea6a
AmiAv01 <amiavproject@gmail.com>
2025-06-14 15:16:32 +0300

    build: new version 1.1.0

--- resources/js/Pages/Desktop/Index.vue
+++ resources/js/Pages/Desktop/Index.vue
@@ -5,14 +5,14 @@
             <div class="flex flex-col">
                 <p class="text-4xl font-bold text-center p-6">Инструкция по установке</p>
                 <div class="flex sm:justify-between justify-center flex-wrap">
-                    <MenuButton v-for="[key, value] of menuItems" :href="`${value}`" :attributes="`px-16 py-6 mr-3 mt-2 sm:mt-0 text-xl font-medium`">
+                    <a v-for="[key, value] of menuItems" :href="`${value}`" :class="`flex items-center text-white bg-green-700  hover:bg-green-800  font-medium rounded-lg  text-center dark:bg-green-600 px-16 py-6 mr-3 mt-2 sm:mt-0 text-xl font-medium`" download>
                         {{key}}
-                    </MenuButton>
+                    </a>
                 </div>
             </div>
             <div>
                 <p class="text-4xl font-bold text-center p-6">Скачать для Windows</p>
-                <MenuButton :attributes="`px-16 py-6 mr-3 text-xl font-medium justify-center `" :href="`/plist.exe`">Скачать</MenuButton>
+                <a :attributes="`px-16 py-6 mr-3 text-xl font-medium justify-center `" :href="`/plist.exe`" download>Скачать</a>
             </div>
         </div>
     </layout>
