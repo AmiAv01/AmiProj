067376523b90d00d0fb12c1a320bd61c10ec8a7b
AmiAv01 <amiavproject@gmail.com>
2025-06-27 19:05:16 +0300

    feat: add images to analogs

--- app/Services/Product/ProductService.php
+++ app/Services/Product/ProductService.php
@@ -20,22 +20,28 @@ class ProductService
         return $this->compatiblePartsService->getCompatibleParts($detailType, $detailInvoice);
     }
 
-    public function getAnalogs(string $id):array
+    public function getAnalogs(string $id): array
     {
+        $analogs = $this->analogService->getAnalogs($id);
+        $analogsWithImages = array_map(function ($item) {
+            $item['imageUrl'] = $this->getImageUrl($item->dt_foto);
+            return $item;
+        }, $analogs);
         return $this->analogService->getAnalogs($id);
     }
 
-    public function getCargoFromAnalogs(array $details):array
+    public function getCargoFromAnalogs(array $details): array
     {
         return $this->analogService->getCargoFromAnalogs($details);
     }
 
-    public function getProductInfoFromOems(string $code): OemInfoDTO{
+    public function getProductInfoFromOems(string $code): OemInfoDTO
+    {
         return $this->oemService->getProductInfoFromOems($code);
     }
 
-    public function getImageUrl(?string $imageName = null):string {
+    public function getImageUrl(?string $imageName = null): string
+    {
         return $this->imageService->getImageUrl($imageName);
     }
-
 }
