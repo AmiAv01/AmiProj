fb76aba942cdc9d10ab033c45446ea4f96b3bf58
AmiAv01 <amiavproject@gmail.com>
2025-07-09 16:20:06 +0300

    fix: fix email styling

--- resources/views/email/order_created.blade.php
+++ resources/views/email/order_created.blade.php
@@ -4,12 +4,12 @@
     <title>АМИ-АВТО Новый заказ</title>
 </head>
 <body>
-<h1>Клиент</h1>
+<h5>Клиент</h5>
 <p>Имя: {{ $order->user->name }}</p>
 <p>Email: {{ $order->user->email }}</p>
 <p>Детали заказа: </p>
 @foreach($order->orderItems as $item)
-    <p>{{$item->detail->dt_invoice}} {{ $item->quantity }} {{ $item->unit_price }}</p>
+    <p>{{$item->detail->dt_invoice}} {{ $item->quantity }} * {{ $item->unit_price }}</p>
 @endforeach
 </body>
 </html>
