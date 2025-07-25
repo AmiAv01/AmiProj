ee6c555c64c33cb64a081ac3e1a4af6ac76d2a43
AmiAv01 <amiavproject@gmail.com>
2025-07-25 12:07:43 +0300

    feat: add title

--- resources/js/Pages/Card/DetailLayout.vue
+++ resources/js/Pages/Card/DetailLayout.vue
@@ -1,36 +1,42 @@
 <template>
-    <div>
-        <div class="grid grid-cols-3 bg-gray-100 p-3 font-semibold border-b">
-            <div class="text-left">Артикул</div>
-            <div class="text-left">Название</div>
-            <div class="text-left">Бренд</div>
-        </div>
+    <div class="rounded-lg border-2 w-full">
+        <p class="text-2xl px-4 py-4 text-center border-b-2">
+            <strong>Деталировка</strong>
+        </p>
         
-        <div 
-            v-for="(item, index) in details" 
-            :key="index"
-            class="grid grid-cols-3 p-3 border-b hover:bg-gray-50"
-        >
-            <div class="flex items-center">
-                <a 
-                    :href="`../../catalog/product/${item.dt_invoice}`"
-                    class="text-blue-600 hover:underline text-sm sm:text-base"
-                >
-                    {{ item.dt_invoice }}
-                </a>
+        <div>
+            <div class="grid grid-cols-3 bg-gray-100 p-3 font-semibold border-b">
+                <div class="text-left">Артикул</div>
+                <div class="text-left">Название</div>
+                <div class="text-left">Бренд</div>
             </div>
             
-            <div class="flex items-center text-sm sm:text-base">
-                {{ item.dt_typec }}
+            <div 
+                v-for="(item, index) in details" 
+                :key="index"
+                class="grid grid-cols-3 p-3 border-b hover:bg-gray-50"
+            >
+                <div class="flex items-center">
+                    <a 
+                        :href="`../../catalog/product/${item.dt_invoice}`"
+                        class="text-blue-600 hover:underline text-sm sm:text-base"
+                    >
+                        {{ item.dt_invoice }}
+                    </a>
+                </div>
+                
+                <div class="flex items-center text-sm sm:text-base">
+                    {{ item.dt_typec }}
+                </div>
+                
+                <div class="flex items-center text-sm sm:text-base">
+                    {{ item.fr_code }}
+                </div>
             </div>
             
-            <div class="flex items-center text-sm sm:text-base">
-                {{ item.fr_code }}
+            <div class="bg-white w-full p-4 flex text-lg text-gray-400 items-center justify-center" v-if="!details.length">
+                <p>Детализация отсутствует</p>
             </div>
-        </div>
-        
-        <div class="bg-white w-full p-4 flex text-lg text-gray-400 items-center justify-center" v-if="!details.length">
-            <p>Детализация отсутствует</p>
         </div>
     </div>
 </template>
