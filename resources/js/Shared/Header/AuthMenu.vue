c880332bc6527f004ad0b9e20aa1653399c8549c
AmiAv01 <amiavproject@gmail.com>
2025-07-25 14:37:56 +0300

    fix: change menu

--- resources/js/Shared/Header/AuthMenu.vue
+++ resources/js/Shared/Header/AuthMenu.vue
@@ -1,47 +1,4 @@
 <template>
-    <div class="flex flex-col">
-        <button
-                type="button"
-                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                id="user-menu-button"
-                aria-expanded="false"
-                data-dropdown-toggle="user-dropdown"
-                data-dropdown-placement="bottom"
-            >
-                <span class="sr-only">Open user menu</span>
-                <i class="fa-solid fa-user text-white text-xl"></i>
-        </button>
-        <p class="block text-sm text-white">{{ $page.props.auth.user.name }}</p>
-    </div>
-    
-    <div
-        class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-        id="user-dropdown"
-    >
-        <div class="px-4 py-3">
-            <span
-                class="block text-sm text-gray-500 truncate dark:text-gray-400"
-            >{{ $page.props.auth.user.email }}</span
-            >
-        </div>
-        <ul class="py-2" aria-labelledby="user-menu-button">
-            <li>
-                <inertia-link
-                    :href="`${routes.get('profile.edit')}`"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >Настройки</inertia-link
-                >
-            </li>
-            <li>
-                <inertia-link
-                    :href="`${routes.get('logout')}`"
-                    method="post"
-                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                >Выход</inertia-link
-                >
-            </li>
-        </ul>
-    </div>
     <menu-button
         :attributes="`px-4 py-2 text-sm mr-4`"
         :href="route('cart.index')"
@@ -84,6 +41,49 @@
         </svg>
         Заказы
     </menu-button>
+    <div class="flex">
+        <button
+                type="button"
+                class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                id="user-menu-button"
+                aria-expanded="false"
+                data-dropdown-toggle="user-dropdown"
+                data-dropdown-placement="bottom"
+            >
+                <span class="sr-only">Open user menu</span>
+                <i class="fa-solid fa-user text-white text-xl"></i>
+        </button>
+        <p class="block text-sm text-white">{{ $page.props.auth.user.name }}</p>
+    </div>
+    
+    <div
+        class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+        id="user-dropdown"
+    >
+        <div class="px-4 py-3">
+            <span
+                class="block text-sm text-gray-500 truncate dark:text-gray-400"
+            >{{ $page.props.auth.user.email }}</span
+            >
+        </div>
+        <ul class="py-2" aria-labelledby="user-menu-button">
+            <li>
+                <inertia-link
+                    :href="`${routes.get('profile.edit')}`"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Настройки</inertia-link
+                >
+            </li>
+            <li>
+                <inertia-link
+                    :href="`${routes.get('logout')}`"
+                    method="post"
+                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                >Выход</inertia-link
+                >
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script setup>
