6de15f0152ee48a2a85e503e900986f5a11de212
AmiAv01 <amiavproject@gmail.com>
2025-07-25 12:00:15 +0300

    fix: fix table

--- resources/js/Pages/Card/DetailLayout.vue
+++ resources/js/Pages/Card/DetailLayout.vue
@@ -1,36 +1,36 @@
 <template>
-    <div class="rounded-lg border-2 w-full">
-        <p class="text-2xl px-4 py-4 text-center border-b-2">
-            <strong>Деталировка</strong>
-        </p>
-        <div class="overflow-x-auto max-h-[400px]">
-            <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-100">
-                    <tr>
-                        <th class="px-4 py-3 text-left text-sm font-medium text-gray-700">Название</th>
-                        <th class="px-4 py-3 text-left text-sm font-medium text-gray-700">Артикул</th>
-                        <th class="px-4 py-3 text-left text-sm font-medium text-gray-700">Бренд</th>
-                    </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
-                    <tr v-for="(item, index) in details" :key="index" class="hover:bg-gray-50">
-                        <td class="px-4 py-3 whitespace-nowrap text-sm font-medium text-gray-900">
-                            {{ item.dt_typec }}
-                        </td>
-                        <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-600">
-                            {{ item.dt_invoice }}
-                        </td>
-                        <td class="px-4 py-3 whitespace-nowrap text-sm text-gray-600">
-                            {{ item.fr_code }}
-                        </td>
-                    </tr>
-                    <tr v-if="!details.length">
-                        <td colspan="3" class="px-4 py-4 text-center text-gray-400 text-sm">
-                            Детализация отсутствует
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+    <div>
+        <div class="grid grid-cols-3 bg-gray-100 p-3 font-semibold border-b">
+            <div class="text-left">Артикул</div>
+            <div class="text-left">Название</div>
+            <div class="text-left">Бренд</div>
+        </div>
+        
+        <div 
+            v-for="(item, index) in details" 
+            :key="index"
+            class="grid grid-cols-3 p-3 border-b hover:bg-gray-50"
+        >
+            <div class="flex items-center">
+                <a 
+                    :href="`../../catalog/product/${item.dt_invoice}`"
+                    class="text-blue-600 hover:underline text-sm sm:text-base"
+                >
+                    {{ item.dt_invoice }}
+                </a>
+            </div>
+            
+            <div class="flex items-center text-sm sm:text-base">
+                {{ item.dt_typec }}
+            </div>
+            
+            <div class="flex items-center text-sm sm:text-base">
+                {{ item.fr_code }}
+            </div>
+        </div>
+        
+        <div class="bg-white w-full p-4 flex text-lg text-gray-400 items-center justify-center" v-if="!details.length">
+            <p>Детализация отсутствует</p>
         </div>
     </div>
 </template>
