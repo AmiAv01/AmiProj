46c7657cd36af926ca2722d349e4bb25fb1bd9ed
AmiAv01 <amiavproject@gmail.com>
2025-08-20 10:07:26 +0300

    fix: revert fix (issue with product component render)

--- app/Http/Controllers/Search/CatalogSearchedController.php
+++ app/Http/Controllers/Search/CatalogSearchedController.php
@@ -7,7 +7,6 @@ use App\DTO\SearchQueryDTO;
 use App\Http\Requests\SearchCatalogFormRequest;
 use App\Services\DetailService;
 use App\Services\FirmService;
-use App\Services\Product\ProductViewService;
 use App\Services\SearchService;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -15,26 +14,17 @@ use Illuminate\Support\Facades\Log;
 
 class CatalogSearchedController extends BaseSearchController
 {
-    public function __construct(protected SearchService $searchService, protected FirmService $firmService, protected DetailService $detailService, protected ProductViewService $productViewService)
+    public function __construct(protected SearchService $searchService, protected FirmService $firmService, protected DetailService $detailService)
     {
         parent::__construct($this->searchService);
     }
 
     public function index(SearchCatalogFormRequest $request): Response
     {
+        Log::info($request);
         $search = $this->getSearchQuery($request);
-        $searchResult = $this->searchService->getBySearchingWithPagination(new SearchQueryDTO($search));
-        if ($searchResult->count() === 1) {
-            $detail = $searchResult->first();
-            $viewData = $this->productViewService->getViewDataForProduct($detail->id_or_appropriate_identifier);
-            $viewName = $this->productViewService->resolveViewName();
-
-            return Inertia::render($viewName, $viewData);
-        }
-
-
         return Inertia::render('SearchedCatalog/SearchedCatalog', [
-            'details' => $searchResult,
+            'details' => $this->searchService->getBySearchingWithPagination(new SearchQueryDTO($search)),
             'title' => "Поиск по $search",
             'categories' => ['brands' => $this->firmService->getAll()],
             'clientBrands' => $this->detailService->getClientBrands(new FilterDTO($request->validated('filter'))),
